33009ee7938db9d8fc3e8749d0c0dbdc2ad1a99a
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-11 20:46:25 +0100

    Add files via upload

--- Test/artemis_test.go
+++ Test/artemis_test.go
@@ -4,55 +4,24 @@ import (
 	"os"
 	"testing"
 	"path/filepath"
+    "net/http"
+    . "github.com/onsi/ginkgo/v2"
 
-	"github.com/onsi/ginkgo"
-	"github.com/onsi/gomega"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/rest"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    . "github.com/onsi/gomega"
+
 )
 
-var _ = ginkgo.Describe("Artemis Broker Setup", func() {
-	ginkgo.It("should have three brokers running", func() {
-		config, err := loadKubeConfig()
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-		clientset, err := kubernetes.NewForConfig(config)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+var _ = Describe("URL Test", func() {
 
-		podList, err := clientset.CoreV1().Pods("default").List(ginkgo.Context, metav1.ListOptions{
-			LabelSelector: "app=artemis-broker",
-		})
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+    It("should visit testkube.io and return 200", func() {
 
-		gomega.Expect(len(podList.Items)).To(gomega.Equal(3), "Expected 3 Artemis brokers, but found %d", len(podList.Items))
-	})
+        resp, err := http.Get("https://testkube.io")
+
+        Expect(err).To(BeNil())
+
+        Expect(resp.StatusCode).To(Equal(200))
+
+    })
+
 })
-
-func TestArtemis(t *testing.T) {
-	RegisterFailHandler(Fail)
-	ginkgo.RunSpecs(t, "Artemis Suite")
-}
-
-func loadKubeConfig() (*rest.Config, error) {
-	var kubeconfig string
-	if home := homeDir(); home != "" {
-		kubeconfig = filepath.Join(home, ".kube", "config")
-	} else {
-		return nil, fmt.Errorf("home directory not found")
-	}
-
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		return nil, err
-	}
-	return config, nil
-}
-
-func homeDir() string {
-	if h := os.Getenv("HOME"); h != "" {
-		return h
-	}
-	return os.Getenv("USERPROFILE")
-}
