772d1f894b4baaaccd4a4bd4bc4f8bcb079840ed
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-11 20:53:00 +0100

    Add files via upload

--- Test/artemis_test.go
+++ Test/artemis_test.go
@@ -11,13 +11,13 @@ import (
 
 var _ = Describe("URL Test", func() {
 
-    It("should visit testkube.io and return 200", func() {
+    It("should visit testkube.io and return 301", func() {
 
         resp, err := http.Get("https://testkube.io")
 
         Expect(err).To(BeNil())
 
-        Expect(resp.StatusCode).To(Equal(200))
+        Expect(resp.StatusCode).To(Equal(301))
 
     })
 
