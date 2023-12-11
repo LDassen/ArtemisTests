801653bd8f21ba4aef4dcc7736d6fcd4ea977d13
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-11 20:26:48 +0100

    Add files via upload

--- Test/artemis_test.go
+++ Test/artemis_test.go
@@ -1,40 +1,32 @@
 package test_test
 
 import (
-	"context"
-	"fmt"
 	"os"
-	"path/filepath"
 	"testing"
+	"path/filepath"
 
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/rest"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = ginkgo.Describe("Artemis Broker Setup", func() {
-	g := gomega.NewGomegaWithT(ginkgo.GinkgoT())
-
-	// Your test goes here
 	ginkgo.It("should have three brokers running", func() {
-		// Load Kubernetes config
 		config, err := loadKubeConfig()
-		g.Expect(err).NotTo(gomega.HaveOccurred())
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-		// Create Kubernetes client
 		clientset, err := kubernetes.NewForConfig(config)
-		g.Expect(err).NotTo(gomega.HaveOccurred())
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-		// Get Artemis broker pods
-		podList, err := clientset.CoreV1().Pods("default").List(context.TODO(), metav1.ListOptions{
+		podList, err := clientset.CoreV1().Pods("default").List(ginkgo.Context, metav1.ListOptions{
 			LabelSelector: "app=artemis-broker",
 		})
-		g.Expect(err).NotTo(gomega.HaveOccurred())
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-		// Assert that there are exactly 3 Artemis broker pods
-		g.Expect(len(podList.Items)).To(gomega.Equal(3), "Expected 3 Artemis brokers, but found %d", len(podList.Items))
+		gomega.Expect(len(podList.Items)).To(gomega.Equal(3), "Expected 3 Artemis brokers, but found %d", len(podList.Items))
 	})
 })
 
@@ -50,13 +42,9 @@ func loadKubeConfig() (*rest.Config, error) {
 		return nil, fmt.Errorf("home directory not found")
 	}
 
-	config, err := rest.InClusterConfig()
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
-		// If running outside the cluster, use kubeconfig
-		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
-		if err != nil {
-			return nil, err
-		}
+		return nil, err
 	}
 	return config, nil
 }
