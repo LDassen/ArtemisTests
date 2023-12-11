d19843dd296898c8bbed4e86954c45ca85a9f87a
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-11 16:14:29 +0100

    Add files via upload

---
+++ Tests/artemis_test.go
@@ -0,0 +1,58 @@
+package test_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"tests"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var _ = Describe("Pod Count in Namespace", func() {
+	var (
+		clientset *kubernetes.Clientset
+		namespace string
+	)
+
+	BeforeSuite(func() {
+		// Load Kubernetes config from default location or provide your kubeconfig path
+		kubeconfig := os.Getenv("KUBECONFIG")
+		if kubeconfig == "" {
+			kubeconfig = os.Getenv("HOME") + "/.kube/config"
+		}
+
+		// Build Kubernetes client
+		config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+		Expect(err).NotTo(HaveOccurred())
+
+		clientset, err = kubernetes.NewForConfig(config)
+		Expect(err).NotTo(HaveOccurred())
+
+		// Set the namespace you want to test
+		namespace = "your-namespace"
+	})
+
+	It("should have the expected number of pods", func() {
+		podList, err := clientset.CoreV1().Pods(namespace).List(metav1.ListOptions{})
+		Expect(err).NotTo(HaveOccurred())
+
+		// Define the expected number of pods
+		expectedPodCount := 3
+
+		// Assert the actual number of pods matches the expected count
+		Expect(len(podList.Items)).To(Equal(expectedPodCount))
+	})
+})
+
+func TestPodCount(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Pod Count Suite")
+}
