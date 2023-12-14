c71ea11a54108516cc60c95b4d47d072fc97e264
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-14 13:55:01 +0100

    Update loadTest.js

--- K6/loadTest.js
+++ K6/loadTest.js
@@ -1,69 +1,40 @@
-import { check, sleep } from 'k6';
+import { check, group } from 'k6';
 import http from 'k6/http';
 
+const BASE_URL = 'http://ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619';
+
 export const options = {
-  vus: 10,
-  duration: '30s',
+  stages: [
+    { duration: '2m', target: 50 }, // Ramp up to 50 virtual users over 2 minutes
+    { duration: '5m', target: 50 }, // Stay at 50 virtual users for 5 minutes
+    { duration: '2m', target: 0 },  // Ramp down to 0 virtual users over 2 minutes
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<500'], // 95% of requests must complete within 500ms
+  },
 };
 
+const credentials = { username: 'cgi', password: 'cgi' };
+const queueName = 'TESTKUBE'; // Replace with the actual queue name
+
 export default function () {
-  const url = 'http://ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619';
-  const queueName = 'TESTKUBE';
-  const message = 'Hello, Core!';
-  const user = 'amq';
-  const password = 'amq';
+  group('Send Message to Queue', () => {
+    // Prepare the message payload
+    const messagePayload = JSON.stringify({ key: 'value' });
 
-  const payload = JSON.stringify({
-    body: message,
+    // Send a message to the queue
+    const sendMessageResponse = http.post(
+      `${BASE_URL}/queues/${queueName}/send`,
+      messagePayload,
+      { auth: credentials }
+    );
+
+    // Check if the request was successful
+    check(sendMessageResponse, {
+      'Message Sent Successfully': (resp) => resp.status === 200,
+    });
+
+    // Sleep for a short duration to simulate some processing time
+    sleep(0.5);
   });
-
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': `Basic ${customEncodeBase64(`${user}:${password}`)}`,
-  };
-
-  const response = http.post(`${url}/queues/${queueName}`, payload, { headers });
-  console.log(response.status, response.body);
-  console.log(`HTTP Request: ${JSON.stringify({ url: `${url}/queues/${queueName}`, payload, headers }, null, 2)}`);
-  console.log(`HTTP Response: ${JSON.stringify(response, null, 2)}`);
-  console.log(`HTTP Response Status Code: ${response.status}`);
-
-  check(response, {
-    'HTTP Request Successful': (r) => r.status === 200,
-    // You may need to adjust the check based on the actual response structure
-    'Core Message Received Successfully': (r) => r.json('receivedMessage') !== null,
-  });
-
-  // Sleep for a short duration between requests (adjust as needed)
-  sleep(1);
-}
-
-function customEncodeBase64(str) {
-  const keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-  let output = '';
-  let chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-  let i = 0;
-
-  while (i < str.length) {
-    chr1 = str.charCodeAt(i++);
-    chr2 = str.charCodeAt(i++);
-    chr3 = str.charCodeAt(i++);
-
-    enc1 = chr1 >> 2;
-    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-    enc4 = chr3 & 63;
-
-    if (isNaN(chr2)) {
-      enc3 = enc4 = 64;
-    } else if (isNaN(chr3)) {
-      enc4 = 64;
-    }
-
-    output = output +
-      keyStr.charAt(enc1) + keyStr.charAt(enc2) +
-      keyStr.charAt(enc3) + keyStr.charAt(enc4);
-  }
-
-  return output;
 }
