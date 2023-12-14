9b2eaa116fa5889abf0a06145d49a58b7ed90290
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-14 09:25:40 +0100

    Update loadTest.js

--- K6/loadTest.js
+++ K6/loadTest.js
@@ -1,33 +1,11 @@
 import { check } from 'k6';
-import { AMQP, Core } from 'k6/x/amqp';
+import { Core } from 'k6/x/amqp';
 import { SharedArray } from 'k6/data';
 
-// Define the Artemis broker connection details
-const AMQP_CONFIG = {
-  url: 'amqp://cgi:cgi@ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619',
-};
-
+// Define the Artemis broker connection details for Core protocol
 const CORE_CONFIG = {
   url: 'tcp://ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619',
 };
-
-// Function to send and receive messages using AMQP
-function sendAndReceiveAMQPMessage(session) {
-  const queueName = 'exampleQueueAMQP';
-  const message = 'Hello, AMQP!';
-
-  // Send message
-  const sendResult = session.send(queueName, message);
-  check(sendResult, {
-    'AMQP Message Sent Successfully': (r) => r === 0,
-  });
-
-  // Receive message
-  const receiveResult = session.receive(queueName);
-  check(receiveResult, {
-    'AMQP Message Received Successfully': (r) => r !== null,
-  });
-}
 
 // Function to send and receive messages using Core
 function sendAndReceiveCoreMessage(session) {
@@ -52,15 +30,6 @@ export const options = {
   duration: '30s',
 };
 
-const amqpSessions = new SharedArray('amqpSessions', function () {
-  const sessions = [];
-  for (let i = 0; i < options.vus; i++) {
-    const amqpSession = new AMQP(AMQP_CONFIG);
-    sessions.push(amqpSession);
-  }
-  return sessions;
-});
-
 const coreSessions = new SharedArray('coreSessions', function () {
   const sessions = [];
   for (let i = 0; i < options.vus; i++) {
@@ -72,7 +41,6 @@ const coreSessions = new SharedArray('coreSessions', function () {
 
 export default function () {
   for (let i = 0; i < options.vus; i++) {
-    sendAndReceiveAMQPMessage(amqpSessions[i]);
     sendAndReceiveCoreMessage(coreSessions[i]);
   }
 }
