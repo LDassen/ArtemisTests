faab6cb480b5947ad943a3f1a4e14342fc686403
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-14 13:02:22 +0100

    Update loadTest.js

--- K6/loadTest.js
+++ K6/loadTest.js
@@ -7,7 +7,7 @@ export const options = {
 };
 
 export default function () {
-  const url = 'http://ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619';  // Adjust the URL according to your Artemis setup
+  const url = 'http://ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619';
   const queueName = 'exampleQueueCore';
   const message = 'Hello, Core!';
   const username = 'cgi';
@@ -16,14 +16,11 @@ export default function () {
   const payload = JSON.stringify({
     queueName,
     message,
-    user: {
-      username,
-      password,
-    },
   });
 
   const headers = {
     'Content-Type': 'application/json',
+    'Authorization': `Basic ${customEncodeBase64(`${username}:${password}`)}`,
   };
 
   const response = http.post(`${url}/send-receive-endpoint`, payload, { headers });
@@ -36,4 +33,34 @@ export default function () {
 
   // Sleep for a short duration between requests (adjust as needed)
   sleep(1);
+}
+
+function customEncodeBase64(str) {
+  const keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+  let output = '';
+  let chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+  let i = 0;
+
+  while (i < str.length) {
+    chr1 = str.charCodeAt(i++);
+    chr2 = str.charCodeAt(i++);
+    chr3 = str.charCodeAt(i++);
+
+    enc1 = chr1 >> 2;
+    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+    enc4 = chr3 & 63;
+
+    if (isNaN(chr2)) {
+      enc3 = enc4 = 64;
+    } else if (isNaN(chr3)) {
+      enc4 = 64;
+    }
+
+    output = output +
+      keyStr.charAt(enc1) + keyStr.charAt(enc2) +
+      keyStr.charAt(enc3) + keyStr.charAt(enc4);
+  }
+
+  return output;
 }
