a970ab14c172c98c8c92db7d842b0013712c180e
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-14 14:00:45 +0100

    Update loadTest.js

--- K6/loadTest.js
+++ K6/loadTest.js
@@ -1,13 +1,11 @@
-import { check, group, sleep } from 'k6';
+import { check, group } from 'k6';
 import http from 'k6/http';
 
 const BASE_URL = 'http://ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619';
 
 export const options = {
   stages: [
-    { duration: '2m', target: 50 }, // Ramp up to 50 virtual users over 2 minutes
-    { duration: '5m', target: 50 }, // Stay at 50 virtual users for 5 minutes
-    { duration: '2m', target: 0 },  // Ramp down to 0 virtual users over 2 minutes
+    { duration: 'm', target: 50 } // Ramp up to 50 virtual users over 2 minutes
   ],
   thresholds: {
     http_req_duration: ['p(95)<500'], // 95% of requests must complete within 500ms
@@ -22,12 +20,16 @@ export default function () {
     // Prepare the message payload
     const messagePayload = JSON.stringify({ key: 'value' });
 
+    console.log('Sending message to the queue:', queueName);
+
     // Send a message to the queue
     const sendMessageResponse = http.post(
       `${BASE_URL}/queues/${queueName}/send`,
       messagePayload,
       { auth: credentials }
     );
+
+    console.log('Response status:', sendMessageResponse.status);
 
     // Check if the request was successful
     check(sendMessageResponse, {
