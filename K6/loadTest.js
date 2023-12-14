f9328214c2f17d67aca973dbe659c95c3143564f
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-14 11:08:34 +0100

    Update loadTest.js

--- K6/loadTest.js
+++ K6/loadTest.js
@@ -1,46 +1,26 @@
 import { check } from 'k6';
-import { Core } from 'activemq-artemis';
-import { SharedArray } from 'k6/data';
-
-// Define the Artemis broker connection details for Core protocol
-const CORE_CONFIG = {
-  url: 'tcp://ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619',
-};
-
-// Function to send and receive messages using Core
-function sendAndReceiveCoreMessage(session) {
-  const queueName = 'exampleQueueCore';
-  const message = 'Hello, Core!';
-
-  // Send message
-  const sendResult = session.send(queueName, message);
-  check(sendResult, {
-    'Core Message Sent Successfully': (r) => r === 0,
-  });
-
-  // Receive message
-  const receiveResult = session.receive(queueName);
-  check(receiveResult, {
-    'Core Message Received Successfully': (r) => r !== null,
-  });
-}
 
 export const options = {
   vus: 10,
   duration: '30s',
 };
 
-const coreSessions = new SharedArray('coreSessions', function () {
-  const sessions = [];
-  for (let i = 0; i < options.vus; i++) {
-    const coreSession = new Core(CORE_CONFIG);
-    sessions.push(coreSession);
-  }
-  return sessions;
-});
-
 export default function () {
-  for (let i = 0; i < options.vus; i++) {
-    sendAndReceiveCoreMessage(coreSessions[i]);
-  }
+  const url = 'ws://ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619';
+  const queueName = 'exampleQueueCore';
+  const message = 'Hello, Core!';
+
+  const response = check(
+    ws.connect(url, {}, (socket) => {
+      // Send message
+      socket.send(JSON.stringify({ queueName, message }));
+
+      // Receive message
+      const receivedMessage = socket.recv();
+      check(receivedMessage, {
+        'Core Message Received Successfully': (r) => r !== null,
+      });
+    }),
+    { 'WebSocket Connection Established': (r) => r && r.code === 101 }
+  );
 }
