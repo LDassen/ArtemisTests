33afa287c1075dda2d73305d195abb277cd5a158
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-15 15:19:16 +0100

    Update loadTestCF.js

--- K6/loadTestCF.js
+++ K6/loadTestCF.js
@@ -1,24 +1,18 @@
-import { check, group, sleep } from 'k6';
-import { ConnectionFactory, TextMessage } from 'k6/x/jms';
+import { group, sleep } from 'k6';
 import { randomString } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
 
-const BASE_URL = 'tcp://10.204.1.8:61616'; // Adjust the protocol and IP as needed
+const BASE_URL = 'http://10.204.1.8:61616'; // Adjust the protocol and IP as needed
 const credentials = { username: 'artemis', password: 'artemis' };
 const queueName = 'TESTKUBE'; // Replace with the actual queue name
 
-const connectionFactory = new ConnectionFactory({
-  brokerURL: BASE_URL,
-  credentials: credentials,
-});
-
 export const options = {
   stages: [
-    { duration: '1m', target: 50 }, // Ramp up to 50 virtual users over 1 minute
-    { duration: '1m', target: 50 }, // Stay at 50 virtual users for 1 minute
-    { duration: '1m', target: 0 },  // Ramp down to 0 virtual users over 1 minute
+    { duration: '1m', target: 50 },
+    { duration: '1m', target: 50 },
+    { duration: '1m', target: 0 },
   ],
   thresholds: {
-    http_req_duration: ['p(95)<500'], // 95% of requests must complete within 500ms
+    http_req_duration: ['p(95)<500'],
   },
 };
 
@@ -29,21 +23,13 @@ export default function () {
 
     console.log('Sending message to the queue:', queueName, 'Message:', messagePayload);
 
-    // Send a message to the queue using JMS
-    const connection = connectionFactory.createConnection();
-    connection.start();
+    // Simulate sending a message to the queue using HTTP
+    const sendMessageResponse = http.post(
+      `${BASE_URL}/jolokia/exec/org.apache.activemq.artemis:broker=\"artemis-broker\",component=addresses,address=\"TESTKUBE\"/sendMessage(java.lang.String)`,
+      { timeout: '60s', auth: credentials }
+    );
 
-    try {
-      const session = connection.createSession();
-      const producer = session.createProducer(session.createQueue(queueName));
-
-      const message = new TextMessage(messagePayload);
-      producer.send(message);
-
-      console.log('Message sent successfully.');
-    } finally {
-      connection.close();
-    }
+    console.log('Response status:', sendMessageResponse.status);
 
     // Sleep for a short duration to simulate some processing time
     sleep(0.5);
