ed85b5d2def34c70881e8dcf17c05640e67a6a5f
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-15 16:18:25 +0100

    Update loadTest.js

--- K6/loadTest.js
+++ K6/loadTest.js
@@ -1,7 +1,7 @@
 import { check, group, sleep } from 'k6';
 import http from 'k6/http';
 
-const BASE_URL = 'http://10.204.0.36:61616';
+const BASE_URL = 'http://10.204.0.53:61616';
 
 export const options = {
   stages: [
