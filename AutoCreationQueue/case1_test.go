9dac3718db2e7217d561dacfce1126f9c210b0d0
Laurent <ldassen1998@gmail.com>
2023-12-18 13:59:50 +0100

    ath

--- AutoCreationQueue/case1_test.go
+++ AutoCreationQueue/case1_test.go
@@ -1,44 +1,26 @@
 package AutoCreationQueue_test
 
 import (
-	"os/exec"
-	"testing"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/kubeshop/testkube/cmd/kubectl-testkube/commands"
+	"github.com/kubeshop/testkube/cmd/kubectl-testkube/commands/common"
+	"os/exec"
 )
 
-var _ = Describe("Artemis Broker", func() {
+var _ = Describe("ActiveMQ Artemis Brokers Pods", func() {
+	const namespace = "activemq-artemis-brokers"
 
-	It("should run a command inside the Artemis broker", func() {
-		// Replace this command with the actual command you want to run inside the broker
-		commandToRun := "./amq-broker/bin/artemis producer --user cgi --password cgi --url tcp://ex-aao-hdls-svc.activemq-artemis-brokers:61616 --message-count 100"
+	Context("When querying ActiveMQ Artemis Brokers pods", func() {
+		It("Should retrieve a list of pods", func() {
+			cmd := exec.Command("kubectl", "get", "pods", "-n", namespace)
+			session, err := commands.Execute(cmd)
+			Expect(err).NotTo(HaveOccurred())
 
-		// Run the command inside the Artemis broker
-		output, err := runCommandInsideBroker(commandToRun)
-		Expect(err).NotTo(HaveOccurred())
+			Expect(session.ExitCode()).To(Equal(0), "kubectl command failed. Output:\n%s", session.Out.Contents())
 
-		// Add your assertions based on the command output
-		Expect(output).To(ContainSubstring("expected-output"))
+			// Add more assertions as needed based on the output
+			// For example, you can use Gomega matchers to check the expected output or count of pods.
+		})
 	})
 })
-
-// Helper function to run a command inside the Artemis broker
-func runCommandInsideBroker(command string) (string, error) {
-	// Replace this with the actual Artemis broker URL and credentials
-	brokerURL := "tcp://10.204.0.36:61616"
-	username := "cgi"
-	password := "cgi"
-
-	// Construct the Artemis command to run a command inside the broker
-	artemisCmd := exec.Command("artemis", "producer", "--message", command, "--destination", "exampleQueue", "--url", brokerURL, "--user", username, "--password", password)
-
-	// Run the Artemis command and capture the output
-	output, err := artemisCmd.CombinedOutput()
-	return string(output), err
-}
-
-func TestArtemis(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Artemis Suite")
-}
