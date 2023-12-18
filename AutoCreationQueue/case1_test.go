013ed0b78d06ff1292d7eeb239e5918d002883e9
Laurent <ldassen1998@gmail.com>
2023-12-18 17:32:57 +0100

    Update case1_test.go

--- AutoCreationQueue/case1_test.go
+++ AutoCreationQueue/case1_test.go
@@ -1,25 +1,94 @@
 package AutoCreationQueue_test
 
 import (
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"github.com/kubeshop/testkube/cmd/kubectl-testkube/commands"
-	"os/exec"
+    "bytes"
+    "context"
+    "path/filepath"
+
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/client-go/kubernetes"
+    "k8s.io/client-go/rest"
+    "k8s.io/client-go/tools/clientcmd"
+    "k8s.io/client-go/util/homedir"
+    "k8s.io/client-go/kubernetes/scheme"
+    "k8s.io/client-go/util/exec"
+    "k8s.io/api/core/v1"
+
+    . "github.com/onsi/ginkgo/v2"
+    . "github.com/onsi/gomega"
 )
 
-var _ = Describe("ActiveMQ Artemis Brokers Pods", func() {
-	const namespace = "activemq-artemis-brokers"
+var _ = Describe("Artemis Test", func() {
+    var (
+        config *rest.Config
+        clientset *kubernetes.Clientset
+    )
 
-	Context("When querying ActiveMQ Artemis Brokers pods", func() {
-		It("Should retrieve a list of pods", func() {
-			cmd := exec.Command("kubectl", "get", "pods", "-n", namespace)
-			session, err := commands.Execute(cmd)
-			Expect(err).NotTo(HaveOccurred())
+    BeforeEach(func() {
+        // Setup Kubernetes config and client
+        var kubeconfig string
+        if home := homedir.HomeDir(); home != "" {
+            kubeconfig = filepath.Join(home, ".kube", "config")
+        }
+        config, _ = clientcmd.BuildConfigFromFlags("", kubeconfig)
+        clientset, _ = kubernetes.NewForConfig(config)
+    })
 
-			Expect(session.ExitCode()).To(Equal(0), "kubectl command failed. Output:\n%s", session.Out.Contents())
+    It("should send messages and check queue", func() {
+        // Find the Artemis broker pod
+        pods, err := clientset.CoreV1().Pods("activemq-artemis-brokers").List(context.TODO(), metav1.ListOptions{
+            LabelSelector: "application=ex-aao-app", // Using your provided label selector
+        })
+        Expect(err).NotTo(HaveOccurred())
+        Expect(pods.Items).NotTo(BeEmpty())
 
-			// Add more assertions as needed based on the output
-			// For example, you can use Gomega matchers to check the expected output or count of pods.
-		})
-	})
+        podName := pods.Items[0].Name // Assuming you want to use the first pod
+
+        // Execute the Artemis producer command
+        execProducerCmd := []string{"./amq-broker/bin/artemis", "producer", "--user", "cgi", "--password", "cgi", "--url", "tcp://ex-aao-hdls-svc.activemq-artemis-brokers:61616", "--message-count", "100"}
+        _, err = execCommandInPod(clientset, config, podName, "activemq-artemis-brokers", execProducerCmd)
+        Expect(err).NotTo(HaveOccurred())
+
+        // Logic to verify producer command output (if needed)
+
+        // Execute the Artemis queue stat command
+        execQueueStatCmd := []string{"./amq-broker/bin/artemis", "queue", "stat", "--url", "tcp://ex-aao-hdls-svc.activemq-artemis-brokers:61616", "--user", "cgi", "--password", "cgi", "--maxRows", "200", "--clustered"}
+        _, err = execCommandInPod(clientset, config, podName, "activemq-artemis-brokers", execQueueStatCmd)
+        Expect(err).NotTo(HaveOccurred())
+
+        // Logic to parse queueStatOutput and verify if 'TEST' queue has 300 messages
+    })
 })
+
+func execCommandInPod(clientset *kubernetes.Clientset, config *rest.Config, podName, namespace string, command []string) (string, error) {
+    req := clientset.CoreV1().RESTClient().
+        Post().
+        Resource("pods").
+        Name(podName).
+        Namespace(namespace).
+        SubResource("exec").
+        VersionedParams(&v1.PodExecOptions{
+            Command:   command,
+            Container: "ex-aao-ss-0", // Replace with your container name
+            Stdout:    true,
+            Stderr:    true,
+        }, scheme.ParameterCodec)
+
+    exec, err := remotecommand.NewSPDYExecutor(config, "POST", req.URL())
+    if err != nil {
+        return "", err
+    }
+
+    var stdout, stderr bytes.Buffer
+    err = exec.Stream(remotecommand.StreamOptions{
+        Stdout: &stdout,
+        Stderr: &stderr,
+        Tty:    false,
+    })
+
+    if err != nil {
+        return "", err
+    }
+
+    return stdout.String(), nil
+}
