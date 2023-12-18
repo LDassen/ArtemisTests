9d97a74b89468b146db5ca28f7475e2964e7552b
Sabrine <sabrine7b@hotmail.com>
2023-12-18 16:07:57 +0100

    Update deployment_test.sh

--- TestSabrine/deployment_test.sh
+++ TestSabrine/deployment_test.sh
@@ -3,8 +3,23 @@
 # Set the path to your Kubernetes Deployment YAML file
 DEPLOYMENT_FILE="ex-aao.yaml"
 
-# Apply the deployment to the cluster
-kubectl apply -f "${DEPLOYMENT_FILE}"
+# Kubernetes API server URL
+K8S_API_SERVER="https://kubernetes.default"
+
+# Token for authentication (try using service account token first)
+TOKEN_FILE="/var/run/secrets/kubernetes.io/serviceaccount/token"
+TOKEN=""
+
+if [ -f "$TOKEN_FILE" ]; then
+  TOKEN=$(cat "$TOKEN_FILE")
+  echo "Using service account token for authentication."
+else
+  echo "Service account token not found. Make sure the script is running inside a Kubernetes Pod with the necessary permissions."
+  exit 1
+fi
+
+# Apply the deployment to the cluster using cURL
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" --data-binary "@$DEPLOYMENT_FILE" "$K8S_API_SERVER/apis/apps/v1/namespaces/default/deployments"
 
 # Check if the deployment was successful
 if [ $? -eq 0 ]; then
@@ -14,20 +29,12 @@ else
   exit 1
 fi
 
-# Add additional checks or tests as needed, for example, wait for pods to be ready
+# Add additional checks or tests as needed
 # For simplicity, we're using a sleep here, but you might want to implement a more robust wait mechanism.
 sleep 30
 
-# Check if the pods are ready
-if kubectl wait --for=condition=Ready pod --timeout=60s -l app=ex-aao > /dev/null; then
-  echo "Pods are ready. Deployment test passed."
-else
-  echo "Error: Pods are not ready. Deployment test failed."
-  exit 1
-fi
-
-# Clean up (optional): Uncomment the line below if you want to delete the deployment after testing
-# kubectl delete -f "${DEPLOYMENT_FILE}"
+# Check if the pods are ready using cURL or other suitable method
+# ...
 
 # Exit with success
 exit 0
