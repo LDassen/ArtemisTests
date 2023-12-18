58ffda95aede2090da38c634ccc6d476fae958e3
Sabrine <sabrine7b@hotmail.com>
2023-12-18 15:02:35 +0100

    Update multibrokernamespacefail_test.go

--- MultiBrokerSetup/multibrokernamespacefail_test.go
+++ MultiBrokerSetup/multibrokernamespacefail_test.go
@@ -5,6 +5,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"os/exec"
+	"strings"
 	"testing"
 )
 
@@ -22,12 +23,19 @@ var _ = Describe("Artemis Deployment", func() {
 
 		// Example: Get the path to the deployment YAML file in the same directory
 		deploymentFile := filepath.Join(".", "ex-aao.yaml")
-		namespace := "non-existing-namespace"
+		namespace := "nonexistent-namespace"
 		cmd := exec.Command("kubectl", "apply", "-f", deploymentFile, "--namespace="+namespace)
-		err := cmd.Run()
+
+		// Capture the standard output and standard error
+		output, err := cmd.CombinedOutput()
 
 		// Expect an error to occur
 		Expect(err).To(HaveOccurred(), "Expected an error but got none")
+
+		// Expect the specific error message
+		expectedErrorMessage := "the namespace from the provided object"
+		Expect(strings.Contains(string(output), expectedErrorMessage)).To(BeTrue(),
+			"Expected error message not found in output: "+string(output))
 	})
 
 	// Add more test cases as needed
