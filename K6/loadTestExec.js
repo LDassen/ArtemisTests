0c4696c7182d50e4ff9af16e1a951c85e5fba9e0
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-18 09:15:57 +0100

    Update loadTestExec.js

--- K6/loadTestExec.js
+++ K6/loadTestExec.js
@@ -1,16 +1,23 @@
 import http from 'k6/http';
 
-const BASE_URL = 'http://ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619';  // Replace with your Artemis management URL
+const BASE_URL = 'http://ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:8161/jolokia';  // Correct Artemis management URL
 const QUEUE_NAME = 'TESTKUBE';              // Replace with your queue name
 const ARTEMIS_CREDENTIALS = 'cgi:cgi';      // Replace with your credentials
 
 export default function () {
+  // Example JSON payload
+  const messagePayload = {
+    text: 'hi, this is a test',
+  };
+  console.log('Message Payload:', JSON.stringify(messagePayload));
+
   // Send a message to the queue using Artemis management API
   const sendMessageResponse = http.post(
-    `${BASE_URL}/console/jolokia/exec/org.apache.activemq.artemis:broker="10.204.0.36",component=addresses,address="${QUEUE_NAME}",subcomponent=queues,routing-type="anycast",queue="${QUEUE_NAME}"/sendMessage`,
-    null,
+    `${BASE_URL}/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0",component=addresses,address="${QUEUE_NAME}",subcomponent=queues,routing-type="anycast",queue="${QUEUE_NAME}"/sendMessage`,
+    JSON.stringify(messagePayload),
     {
       headers: {
+        'Content-Type': 'application/json',  // Set the content type for JSON payload
         Authorization: `Basic ${ARTEMIS_CREDENTIALS}`,
       },
     }
@@ -19,7 +26,7 @@ export default function () {
   console.log('Request URL:', `${BASE_URL}/console/jolokia/exec/...`);
   console.log('Request Headers:', { Authorization: `Basic ${ARTEMIS_CREDENTIALS}` });
   console.log('Response status:', sendMessageResponse.status);
-  console.log('Response body:', sendMessageResponse.body);  
+  console.log('Response body:', sendMessageResponse.body);
 
   // Check if the request was successful
   if (sendMessageResponse.status === 200) {
