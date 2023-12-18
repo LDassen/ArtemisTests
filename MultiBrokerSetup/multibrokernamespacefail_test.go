9a182e0ec266a2038f5c7062f11f01fe7ed21410
Sabrine <sabrine7b@hotmail.com>
2023-12-18 13:50:56 +0100

    Update multibrokernamespacefail_test.go

--- MultiBrokerSetup/multibrokernamespacefail_test.go
+++ MultiBrokerSetup/multibrokernamespacefail_test.go
@@ -20,18 +20,7 @@ var _ = BeforeSuite(func() {
 	// You can use an underscore (_) to indicate that the variable is intentionally unused
 	_, err = kubernetes.NewForConfig(config)
 	Expect(err).NotTo(HaveOccurred())
-
-	// Download kubectl
-	cmd := exec.Command("curl", "-LO", "https://storage.googleapis.com/kubernetes-release/release/v1.23.0/bin/linux/amd64/kubectl")
-	err = cmd.Run()
-	Expect(err).NotTo(HaveOccurred())
-
-	// Move kubectl to the desired location
-	kubectlPath := "/usr/local/bin/kubectl"
-	_, err = exec.Command("mv", "kubectl", kubectlPath).CombinedOutput()
-	Expect(err).NotTo(HaveOccurred())
 })
-
 
 var _ = Describe("Deploying to Non-existing Namespace", func() {
 	It("Should fail to deploy in a non-existing namespace", func() {
@@ -44,8 +33,10 @@ var _ = Describe("Deploying to Non-existing Namespace", func() {
 		// Construct the full path to the deployment file
 		deploymentFile := filepath.Join(currentDir, "ex-aao.yaml")
 
-		// Use the downloaded kubectl binary within the pod
-		kubectlPath := "/path/to/kubectl"
+		// Use exec.LookPath to find kubectl in the system's PATH
+		kubectlPath, err := exec.LookPath("kubectl")
+		Expect(err).NotTo(HaveOccurred())
+
 		cmd := exec.Command(kubectlPath, "apply", "-f", deploymentFile, "--namespace="+namespace)
 		output, err := cmd.CombinedOutput()
 
@@ -62,35 +53,3 @@ var _ = Describe("Deploying to Non-existing Namespace", func() {
 var _ = AfterSuite(func() {
 	// Clean up resources if needed
 })
-
-// downloadKubectl downloads kubectl binary if not already present
-func downloadKubectl() error {
-	// Check if kubectl is already present
-	kubectlPath, err := exec.LookPath("kubectl")
-	if err == nil {
-		fmt.Println("kubectl is already present:", kubectlPath)
-		return nil
-	}
-
-	// Download kubectl based on the OS
-	var downloadURL string
-	switch runtime.GOOS {
-	case "linux":
-		downloadURL = "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-	case "darwin":
-		downloadURL = "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl"
-	case "windows":
-		downloadURL = "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/windows/amd64/kubectl.exe"
-	default:
-		return fmt.Errorf("unsupported operating system: %s", runtime.GOOS)
-	}
-
-	// Download kubectl
-	downloadCmd := exec.Command("sh", "-c", fmt.Sprintf("curl -LO %s && chmod +x kubectl && mv kubectl /path/to/", downloadURL))
-	downloadOutput, downloadErr := downloadCmd.CombinedOutput()
-	if downloadErr != nil {
-		return fmt.Errorf("failed to download kubectl: %v\nOutput:\n%s", downloadErr, string(downloadOutput))
-	}
-
-	return nil
-}
