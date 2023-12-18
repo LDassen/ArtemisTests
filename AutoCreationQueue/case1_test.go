3e91070bfc73ceb0290b0921336383dce0af5d2d
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-18 11:54:01 +0100

    Update case1_test.go

--- AutoCreationQueue/case1_test.go
+++ AutoCreationQueue/case1_test.go
@@ -3,8 +3,14 @@ package AutoCreationQueue_test
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"os/exec"
 	"testing"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -13,15 +19,19 @@ import (
 var _ = Describe("Artemis Broker", func() {
 
 	It("should run a command inside the Artemis broker", func() {
-		// Replace these values with your actual Kubernetes configuration
-		podName := "artemis-broker-pod-name"
-		namespace := "default"
+		// Create Kubernetes client
+		clientset, err := createKubernetesClient()
+		Expect(err).NotTo(HaveOccurred())
 
 		// Replace this command with the actual command you want to run inside the broker
 		commandToRun := "./amq-broker/bin/artemis producer --user cgi --password cgi --url tcp://10.204.0.39:61616 --message-count 100"
 
+		// Get the pod's name and namespace
+		podName, namespace, err := getPodInfo()
+		Expect(err).NotTo(HaveOccurred())
+
 		// Run the command inside the specific Artemis broker pod
-		output, err := runCommandInsideKubernetesPod(podName, namespace, commandToRun)
+		output, err := runCommandInsideKubernetesPod(clientset, podName, namespace, commandToRun)
 		Expect(err).NotTo(HaveOccurred())
 
 		// Add your assertions based on the command output
@@ -29,10 +39,50 @@ var _ = Describe("Artemis Broker", func() {
 	})
 })
 
+// Helper function to create a Kubernetes client
+func createKubernetesClient() (*kubernetes.Clientset, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return clientset, nil
+}
+
+// Helper function to get pod information (name and namespace)
+func getPodInfo() (string, string, error) {
+	podName := os.Getenv("POD_NAME")
+	if podName == "" {
+		return "", "", fmt.Errorf("POD_NAME environment variable not set")
+	}
+
+	namespace := os.Getenv("POD_NAMESPACE")
+	if namespace == "" {
+		return "", "", fmt.Errorf("POD_NAMESPACE environment variable not set")
+	}
+
+	return podName, namespace, nil
+}
+
 // Helper function to run a command inside a Kubernetes pod using exec
-func runCommandInsideKubernetesPod(podName, namespace, command string) (string, error) {
+func runCommandInsideKubernetesPod(clientset *kubernetes.Clientset, podName, namespace, command string) (string, error) {
+	pod, err := clientset.CoreV1().Pods(namespace).Get(podName, v1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	containerName := pod.Spec.Containers[0].Name
+
 	// Create an exec command
-	execCommand := exec.Command("kubectl", "exec", "-it", "pod", podName, namespace, "--", "/bin/bash", command)
+	execCommand := exec.Command("sh", "-c", command)
+
+	// Set the correct working directory
+	execCommand.Dir = "/home/jboss"
 
 	// Capture the command output
 	var stdout, stderr bytes.Buffer
@@ -40,7 +90,7 @@ func runCommandInsideKubernetesPod(podName, namespace, command string) (string, 
 	execCommand.Stderr = &stderr
 
 	// Run the exec command
-	err := execCommand.Run()
+	err = execCommand.Run()
 	if err != nil {
 		return "", fmt.Errorf("error executing command: %v\nstdout: %s\nstderr: %s", err, stdout.String(), stderr.String())
 	}
