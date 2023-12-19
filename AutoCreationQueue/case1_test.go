4ea11357d4c2b9276fa78b7663d93940edc37324
Laurent <ldassen1998@gmail.com>
2023-12-19 23:03:52 +0100

    ad

--- AutoCreationQueue/case1_test.go
+++ AutoCreationQueue/case1_test.go
@@ -2,12 +2,14 @@ package AutoCreationQueue_test
 
 import (
     "context"
+    "github.com/streadway/amqp"
     . "github.com/onsi/ginkgo/v2"
     . "github.com/onsi/gomega"
 
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     "k8s.io/client-go/kubernetes"
     "k8s.io/client-go/rest"
+    v1 "k8s.io/api/core/v1"
 )
 
 var _ = Describe("Broker Label Test", func() {
@@ -22,20 +24,50 @@ var _ = Describe("Broker Label Test", func() {
         Expect(err).NotTo(HaveOccurred())
         clientset, err = kubernetes.NewForConfig(config)
         Expect(err).NotTo(HaveOccurred())
+
+        // Connect to Artemis using AMQP
+        conn, err := amqp.Dial("amqp://cgi:cgi@ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619")
+        Expect(err).NotTo(HaveOccurred())
+        defer conn.Close()
+
+        ch, err := conn.Channel()
+        Expect(err).NotTo(HaveOccurred())
+        defer ch.Close()
+
+        body := "Hi, this is a test!"
+        err = ch.Publish(
+            "",         // exchange
+            "TESTKUBE", // routing key (queue name)
+            false,      // mandatory
+            false,      // immediate
+            amqp.Publishing{
+                ContentType: "text/plain",
+                Body:        []byte(body),
+            })
+        Expect(err).NotTo(HaveOccurred())
     })
 
-    It("should check for exactly three brokers with the correct labels", func() {
+    It("should execute a command in one of the pods", func() {
         pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
             LabelSelector: "application=ex-aao-app",
         })
         Expect(err).NotTo(HaveOccurred())
 
-        // Check for exactly three brokers
-        Expect(len(pods.Items)).To(Equal(3), "There should be exactly three brokers")
+        // Assuming you pick the first pod for simplicity
+        podName := pods.Items[0].Name
+        cmd := []string{"bin/bash", "-c", "./amq-broker/bin/artemis queue stat --url tcp://ex-aao-hdls-svc.activemq-artemis-brokers:61616 --user cgi --password cgi --maxRows 200"}
 
-        for _, pod := range pods.Items {
-            // Log the pod names
-            GinkgoWriter.Printf("Pod %s has the correct label\n", pod.Name)
-        }
+        req := clientset.CoreV1().Pods(namespace).Exec(podName, &v1.PodExecOptions{
+            Command: cmd,
+            Stdout:  true,
+            Stderr:  true,
+        })
+
+        // Execute the command
+        // Note: You might need to handle the stream (stdout, stderr) to capture the command output
+        _, err = Execute(req, config)
+        Expect(err).NotTo(HaveOccurred())
+
+        // Add your logic here to check for the specific number in the output line with 'TEST'
     })
 })
--- AutoCreationQueue/go.mod
+++ AutoCreationQueue/go.mod
@@ -37,6 +37,7 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/streadway/amqp v1.1.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/oauth2 v0.13.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
