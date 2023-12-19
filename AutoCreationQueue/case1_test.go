76a93f67f92b0a40d0919b235aa75ed47cfbf39e
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-19 23:03:04 +0100

    Update case1_test.go

--- AutoCreationQueue/case1_test.go
+++ AutoCreationQueue/case1_test.go
@@ -2,59 +2,72 @@ package AutoCreationQueue_test
 
 import (
     "context"
-    "fmt"
+    "github.com/streadway/amqp"
     . "github.com/onsi/ginkgo/v2"
     . "github.com/onsi/gomega"
-    "io"
+
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     "k8s.io/client-go/kubernetes"
     "k8s.io/client-go/rest"
-    "k8s.io/client-go/tools/remotecommand"
-    // Import Artemis client package (example, adjust based on actual package)
-    "github.com/zeus-fyi/zeus/pkg/artemis/client"
+    v1 "k8s.io/api/core/v1"
 )
 
-var _ = Describe("Artemis Queue Message Test", func() {
+var _ = Describe("Broker Label Test", func() {
     var (
         clientset *kubernetes.Clientset
         namespace string = "activemq-artemis-brokers"
-        artemisClient *client.Client // Artemis client
     )
 
     BeforeEach(func() {
-        // Set up the Kubernetes client
+        // Set up the client
         config, err := rest.InClusterConfig()
         Expect(err).NotTo(HaveOccurred())
         clientset, err = kubernetes.NewForConfig(config)
         Expect(err).NotTo(HaveOccurred())
 
-        // Set up the Artemis client
-        artemisClient = client.NewClient("ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local", "cgi", "cgi")
+        // Connect to Artemis using AMQP
+        conn, err := amqp.Dial("amqp://cgi:cgi@ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:5672")
+        Expect(err).NotTo(HaveOccurred())
+        defer conn.Close()
+
+        ch, err := conn.Channel()
+        Expect(err).NotTo(HaveOccurred())
+        defer ch.Close()
+
+        body := "Hi, this is a test!"
+        err = ch.Publish(
+            "",         // exchange
+            "TESTKUBE", // routing key (queue name)
+            false,      // mandatory
+            false,      // immediate
+            amqp.Publishing{
+                ContentType: "text/plain",
+                Body:        []byte(body),
+            })
+        Expect(err).NotTo(HaveOccurred())
     })
 
-    It("should send a message to Artemis and check output in pod", func() {
+    It("should execute a command in one of the pods", func() {
         pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
             LabelSelector: "application=ex-aao-app",
         })
         Expect(err).NotTo(HaveOccurred())
 
-        // Send a message to the Artemis queue
-        err = artemisClient.SendMessage("TESTKUBE", "Hi, this is a test!")
+        // Assuming you pick the first pod for simplicity
+        podName := pods.Items[0].Name
+        cmd := []string{"bin/bash", "-c", "./amq-broker/bin/artemis queue stat --url tcp://ex-aao-hdls-svc.activemq-artemis-brokers:61616 --user cgi --password cgi --maxRows 200"}
+
+        req := clientset.CoreV1().Pods(namespace).Exec(podName, &v1.PodExecOptions{
+            Command: cmd,
+            Stdout:  true,
+            Stderr:  true,
+        })
+
+        // Execute the command
+        // Note: You might need to handle the stream (stdout, stderr) to capture the command output
+        _, err = Execute(req, config)
         Expect(err).NotTo(HaveOccurred())
 
-        // Exec into one of the pods and check output
-        req := clientset.CoreV1().Pods(namespace).GetLogs(pods.Items[0].Name, &v1.PodLogOptions{})
-        podLogs, err := req.Stream(context.TODO())
-        Expect(err).NotTo(HaveOccurred())
-        defer podLogs.Close()
-
-        buf := new(bytes.Buffer)
-        _, err = io.Copy(buf, podLogs)
-        Expect(err).NotTo(HaveOccurred())
-        output := buf.String()
-
-        // Check for specific number in line with 'TEST'
-        Expect(output).To(ContainSubstring("TEST"))
-        // Additional checks based on output format
+        // Add your logic here to check for the specific number in the output line with 'TEST'
     })
 })
