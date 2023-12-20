dfdf05adb2fdbf1469237f1b239c6cb4630ea045
Laurent <ldassen1998@gmail.com>
2023-12-20 11:50:02 +0100

    sfvds

--- AutoCreationQueue/case1case2case3_test.go
+++ AutoCreationQueue/case1case2case3_test.go
@@ -24,35 +24,57 @@ var _ = ginkgo.Describe("Artemis Queue Test with AMQP", func() {
         gomega.Expect(err).NotTo(gomega.HaveOccurred())
     })
 
-    ginkgo.It("should send and receive a message in a queue", func() {
+    ginkgo.It("should send and receive two messages in a queue", func() {
         queueName := "TESTKUBE"
-        messageText := "Hello, Artemis!"
-
+        firstMessageText := "Hi, this is message 1"
+        secondMessageText := "Hi, this is message 2"
+    
         // Create a sender
         sender, err = session.NewSender(
             amqp.LinkTargetAddress(queueName),
         )
         gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-        // Send a message
-        err = sender.Send(ctx, amqp.NewMessage([]byte(messageText)))
+    
+        // Send the first message
+        err = sender.Send(ctx, amqp.NewMessage([]byte(firstMessageText)))
         gomega.Expect(err).NotTo(gomega.HaveOccurred())
+    
+        // Wait for 10 minutes after sending the first message
+        time.Sleep(10 * time.Minute)
 
-		// Wait for 10 minutes
-		time.Sleep(1 * time.Minute)
-
-        // Create a receiver
+        // Create a receiver for the first message
         receiver, err = session.NewReceiver(
             amqp.LinkSourceAddress(queueName),
         )
         gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-        // Receive a message
+    
+        // Receive the first message
         msg, err := receiver.Receive(ctx)
         gomega.Expect(err).NotTo(gomega.HaveOccurred())
-        gomega.Expect(string(msg.GetData())).To(gomega.Equal(messageText))
-
-        // Accept message
+        gomega.Expect(string(msg.GetData())).To(gomega.Equal(firstMessageText))
+    
+        // Accept the first message
+        msg.Accept()
+    
+        // Close the first receiver to clean up before receiving the second message
+        receiver.Close(ctx)
+    
+        // Send the second message
+        err = sender.Send(ctx, amqp.NewMessage([]byte(secondMessageText)))
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+    
+        // Create a receiver for the second message
+        receiver, err = session.NewReceiver(
+            amqp.LinkSourceAddress(queueName),
+        )
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+    
+        // Receive the second message
+        msg, err = receiver.Receive(ctx)
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+        gomega.Expect(string(msg.GetData())).To(gomega.Equal(secondMessageText))
+    
+        // Accept the second message
         msg.Accept()
     })
 
