c342d73dd63ed18979f17330be7945b0ccfd494f
Laurent <ldassen1998@gmail.com>
2023-12-20 11:56:08 +0100

    Update case1case2case3_test.go

--- AutoCreationQueue/case1case2case3_test.go
+++ AutoCreationQueue/case1case2case3_test.go
@@ -49,12 +49,12 @@ var _ = ginkgo.Describe("Artemis Queue Test with AMQP", func() {
         gomega.Expect(err).NotTo(gomega.HaveOccurred())
     
         // Receive the first message
-        msg1, err := receiver.Receive(ctx)
+        msg, err := receiver.Receive(ctx)
         gomega.Expect(err).NotTo(gomega.HaveOccurred())
-        gomega.Expect(string(msg1.GetData())).To(gomega.Equal(firstMessageText))
+        gomega.Expect(string(msg.GetData())).To(gomega.Equal(firstMessageText))
     
         // Accept the first message
-        msg1.Accept()
+        msg.Accept()
     
         // Close the first receiver to clean up before receiving the second message
         receiver.Close(ctx)
@@ -70,12 +70,12 @@ var _ = ginkgo.Describe("Artemis Queue Test with AMQP", func() {
         gomega.Expect(err).NotTo(gomega.HaveOccurred())
     
         // Receive the second message
-        msg2, err = receiver.Receive(ctx)
+        msg, err = receiver.Receive(ctx)
         gomega.Expect(err).NotTo(gomega.HaveOccurred())
-        gomega.Expect(string(msg2.GetData())).To(gomega.Equal(secondMessageText))
+        gomega.Expect(string(msg.GetData())).To(gomega.Equal(secondMessageText))
     
         // Accept the second message
-        msg2.Accept()
+        msg.Accept()
     })
 
     ginkgo.AfterEach(func() {
