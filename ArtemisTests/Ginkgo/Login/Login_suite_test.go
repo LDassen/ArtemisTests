a381d2bcf53124bded58277178d9596ed27c8de0
Laurent <ldassen1998@gmail.com>
2023-12-22 10:05:04 +0100

    e

---
+++ ArtemisTests/Ginkgo/Login/Login_suite_test.go
@@ -0,0 +1,13 @@
+package Login_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestTest(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Login Suite")
+}
