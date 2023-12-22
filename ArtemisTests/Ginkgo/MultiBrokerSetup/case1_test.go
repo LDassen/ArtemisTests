2c75ed226e92894a688b62326bfc71ce1f615fad
Sabrine <sabrine7b@hotmail.com>
2023-12-22 09:51:21 +0100

    Update case1_test.go

--- ArtemisTests/Ginkgo/MultiBrokerSetup/case1_test.go
+++ ArtemisTests/Ginkgo/MultiBrokerSetup/case1_test.go
@@ -5,7 +5,7 @@ import (
 	"context"
 	"io/ioutil"
 	"path/filepath"
-
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
