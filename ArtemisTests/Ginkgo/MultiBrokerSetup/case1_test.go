c7a9130c70aaf930e4ec012641c461de89224370
Sabrine <sabrine7b@hotmail.com>
2023-12-22 11:17:09 +0100

    sr5h

--- ArtemisTests/Ginkgo/MultiBrokerSetup/case1_test.go
+++ ArtemisTests/Ginkgo/MultiBrokerSetup/case1_test.go
@@ -33,7 +33,7 @@ var _ = ginkgo.Describe("Kubernetes Apply Deployment Test", func() {
 	})
 
 	ginkgo.It("should apply a deployment file for Artemis to a namespace", func() {
-		fileName := "case_1-ex-aao.yaml"
+		fileName := "ex-aao.yaml" //"case_1-ex-aao.yaml"
 		namespace := "activemq-artemis-brokers" // Replace with your existing namespace or a new one
 
 		// Check if the namespace already exists
