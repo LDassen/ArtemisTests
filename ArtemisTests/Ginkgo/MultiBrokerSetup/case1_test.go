55704ecad0d90480d85954e67890e13ff976c162
Laurent <ldassen1998@gmail.com>
2023-12-22 13:43:03 +0100

    Update case1_test.go

--- ArtemisTests/Ginkgo/MultiBrokerSetup/case1_test.go
+++ ArtemisTests/Ginkgo/MultiBrokerSetup/case1_test.go
@@ -18,9 +18,7 @@ import (
 )
 
 var _ = ginkgo.Describe("ActiveMQ Artemis Deployment Test", func() {
-    var clientset *kubernetes.Clientset
     var dynamicClient dynamic.Interface
-    var resourceName string
     var namespace string
     var resourceGVR schema.GroupVersionResource
 
@@ -29,14 +27,10 @@ var _ = ginkgo.Describe("ActiveMQ Artemis Deployment Test", func() {
         config, err := rest.InClusterConfig()
         gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-        clientset, err = kubernetes.NewForConfig(config)
-        gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
         dynamicClient, err = dynamic.NewForConfig(config)
         gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
         namespace = "activemq-artemis-brokers"
-        resourceName = "ex-aao"
         resourceGVR = schema.GroupVersionResource{
             Group:    "broker.amq.io",
             Version:  "v1beta1",
@@ -55,7 +49,7 @@ var _ = ginkgo.Describe("ActiveMQ Artemis Deployment Test", func() {
 
         decUnstructured := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
         obj := &unstructured.Unstructured{}
-        _, gvk, err := decUnstructured.Decode(fileBytes, nil, obj)
+        _, _, err = decUnstructured.Decode(fileBytes, nil, obj)
         gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
         obj.SetAPIVersion("broker.amq.io/v1beta1")
@@ -69,6 +63,4 @@ var _ = ginkgo.Describe("ActiveMQ Artemis Deployment Test", func() {
         // Confirm that the resource has been created
         fmt.Printf("Created ActiveMQArtemis resource: %s\n", createdObj.GetName())
     })
-
-    // No AfterEach block needed since we are not deleting the resource
 })
