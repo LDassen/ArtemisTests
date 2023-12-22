c2ccb278af40dcb5cc236307aea7ce7d36c3c37e
Sabrine <sabrine7b@hotmail.com>
2023-12-22 11:42:44 +0100

    Update case1_test.go

--- ArtemisTests/Ginkgo/MultiBrokerSetup/case1_test.go
+++ ArtemisTests/Ginkgo/MultiBrokerSetup/case1_test.go
@@ -11,7 +11,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/util/yaml"
@@ -25,10 +25,11 @@ var _ = ginkgo.Describe("Kubernetes Apply Deployment Test", func() {
 	ginkgo.BeforeEach(func() {
 		// Set up the Kubernetes client
 		config, err := rest.InClusterConfig()
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		gomega.Expect(err).To(gomega.BeNil(), "Error getting in-cluster config: %v", err)
 
 		clientset, err = kubernetes.NewForConfig(config)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		gomega.Expect(err).To(gomega.BeNil(), "Error creating Kubernetes client: %v", err)
+		gomega.RegisterFailHandler(ginkgo.Fail)
 	})
 
 	ginkgo.It("should apply a deployment file for Artemis to a namespace", func() {
@@ -37,41 +38,43 @@ var _ = ginkgo.Describe("Kubernetes Apply Deployment Test", func() {
 
 		// Read the file
 		filePath, err := filepath.Abs(fileName)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		gomega.Expect(err).To(gomega.BeNil(), "Error getting absolute file path: %v", err)
 
 		fileBytes, err := ioutil.ReadFile(filePath)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		gomega.Expect(err).To(gomega.BeNil(), "Error reading file: %v", err)
 
 		// Decode the YAML manifest
 		decode := yaml.NewYAMLOrJSONDecoder(bytes.NewReader(fileBytes), 1024)
 		var deployment appsv1.Deployment
 		err = decode.Decode(&deployment)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		gomega.Expect(err).To(gomega.BeNil(), "Error decoding YAML: %v", err)
 
 		// Apply the deployment to the namespace
 		_, err = clientset.AppsV1().Deployments(namespace).Create(context.TODO(), &deployment, metav1.CreateOptions{})
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		gomega.Expect(err).To(gomega.BeNil(), "Error creating deployment: %v", err)
 
 		// Wait for the deployment to be available
 		ginkgo.By("Waiting for the deployment to be available")
 		err = waitForDeployment(clientset, namespace, deployment.Name, 3, 5*time.Minute) // Adjust timeout as needed
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		gomega.Expect(err).To(gomega.BeNil(), "Error waiting for deployment: %v", err)
+	})
+
+	ginkgo.AfterEach(func() {
+		// Cleanup logic if needed
 	})
 
 	ginkgo.It("should have the correct number of 'broker' pods running", func() {
 		config, err := rest.InClusterConfig()
-		gomega.Expect(err).To(BeNil(), "Error getting in-cluster config: %v", err)
+		gomega.Expect(err).To(gomega.BeNil(), "Error getting in-cluster config: %v", err)
 
 		clientset, err := kubernetes.NewForConfig(config)
-		gomega.Expect(err).To(BeNil(), "Error creating Kubernetes client: %v", err)
+		gomega.Expect(err).To(gomega.BeNil(), "Error creating Kubernetes client: %v", err)
 
 		namespace := "activemq-artemis-brokers"
 		expectedPodCount := 3 // Set your expected number of 'broker' pods
 
-		pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
-			LabelSelector: "app=broker",
-		})
-		gomega.Expect(err).To(BeNil(), "Error getting pods: %v", err)
+		pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: "application=ex-aao-app"})
+		gomega.Expect(err).To(gomega.BeNil(), "Error getting pods: %v", err)
 
 		// Debugging statements
 		fmt.Printf("Retrieved %d pods in namespace %s\n", len(pods.Items), namespace)
@@ -83,10 +86,6 @@ var _ = ginkgo.Describe("Kubernetes Apply Deployment Test", func() {
 		actualPodCount := len(pods.Items)
 
 		gomega.Expect(actualPodCount).To(gomega.Equal(expectedPodCount), "Expected %d 'broker' pods, but found %d", expectedPodCount, actualPodCount)
-	})
-
-	ginkgo.AfterEach(func() {
-		// Cleanup logic if needed
 	})
 })
 
