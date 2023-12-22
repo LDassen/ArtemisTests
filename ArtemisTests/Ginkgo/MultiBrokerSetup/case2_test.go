88d74058520f3d1105212c2fdec3dd24a48ca789
Laurent <77445507+LDassen@users.noreply.github.com>
2023-12-22 12:00:01 +0100

    Update case2_test.go

--- ArtemisTests/Ginkgo/MultiBrokerSetup/case2_test.go
+++ ArtemisTests/Ginkgo/MultiBrokerSetup/case2_test.go
@@ -1,63 +1,66 @@
 package MultiBrokerSetup_test
 
 import (
-	"bytes"
-	"context"
-	"io/ioutil"
-	"path/filepath"
-	"fmt"
-	"strings"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"github.com/onsi/ginkgo/v2"
-	"github.com/onsi/gomega"
-	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/apimachinery/pkg/util/yaml"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+    "bytes"
+    "context"
+    "fmt"
+    "io/ioutil"
+    "path/filepath"
+    "strings"
+
+    appsv1 "k8s.io/api/apps/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/apimachinery/pkg/util/yaml"
+    "k8s.io/client-go/kubernetes"
+    "k8s.io/client-go/rest"
+
+    "github.com/onsi/ginkgo/v2"
+    "github.com/onsi/gomega"
 )
 
 var _ = ginkgo.Describe("Kubernetes Apply Deployment Test", func() {
-	var clientset *kubernetes.Clientset
+    var clientset *kubernetes.Clientset
 
-	ginkgo.BeforeEach(func() {
-		// Set up the Kubernetes client
-		config, err := rest.InClusterConfig()
-		if err != nil {
-			gomega.Expect(err).NotTo(gomega.HaveOccurred())
-		}
+    ginkgo.BeforeEach(func() {
+        // Set up the Kubernetes client
+        config, err := rest.InClusterConfig()
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-		clientset, err = kubernetes.NewForConfig(config)
-		if err != nil {
-			if strings.Contains(err.Error(), "the namespace of the provided object does not match the namespace sent on the request") {
-				fmt.Println("[ERROR] Non-existing namespace error encountered:", err)
-				ginkgo.Skip("Non-existing namespace error: aborting test")
-			}
-			gomega.Expect(err).NotTo(gomega.HaveOccurred())
-		}
-	})
+        clientset, err = kubernetes.NewForConfig(config)
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+    })
 
-	ginkgo.It("Should fail to apply a deployment file for Artemis to a non-existing namespace", func() {
-		fileName := "ex-aao.yaml"
-		namespace := "non-existing"
+    ginkgo.It("Should fail to apply a deployment file for Artemis to a non-existing namespace", func() {
+        fileName := "ex-aao.yaml"
+        namespace := "non-existing"
 
-		// Read the file
-		filePath, err := filepath.Abs(fileName)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+        // Read the file
+        filePath, err := filepath.Abs(fileName)
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-		fileBytes, err := ioutil.ReadFile(filePath)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+        fileBytes, err := ioutil.ReadFile(filePath)
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-		// Decode the YAML manifest
-		decode := yaml.NewYAMLOrJSONDecoder(bytes.NewReader(fileBytes), 1024)
-		var deployment appsv1.Deployment
-		err = decode.Decode(&deployment)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+        // Decode the YAML manifest
+        decode := yaml.NewYAMLOrJSONDecoder(bytes.NewReader(fileBytes), 1024)
+        var deployment appsv1.Deployment
+        err = decode.Decode(&deployment)
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-		// Try to apply the deployment to the non-existing namespace
-		_, err = clientset.AppsV1().Deployments(namespace).Create(context.TODO(), &deployment, metav1.CreateOptions{})
-	})
+        // Try to apply the deployment to the non-existing namespace
+        _, err = clientset.AppsV1().Deployments(namespace).Create(context.TODO(), &deployment, metav1.CreateOptions{})
+        if err != nil && strings.Contains(err.Error(), "the namespace of the provided object does not match the namespace sent on the request") {
+            fmt.Println("[ERROR] Namespace mismatch error encountered:", err)
+            ginkgo.Skip("Namespace mismatch error: skipping test due to specific error")
+        }
 
-	ginkgo.AfterEach(func() {
-		// Cleanup logic if needed
-	})
+        gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Deployment should not succeed in non-existing namespace")
+    })
+
+    ginkgo.AfterEach(func() {
+		err := clientset.AppsV1().Deployments("activemq-artemis-brokers").Delete(context.TODO(), "ex-aao", metav1.DeleteOptions{})
+		gomega.Expect(err).To(gomega.BeNil(), "Error deleting deployment: %v", err)
+    })
 })
+
+
