be731a9a681eebc00475c09fd06aa4399ffc244d
Laurent <ldassen1998@gmail.com>
2023-12-22 11:18:34 +0100

    Update case13_test.go

--- ArtemisTests/Ginkgo/Login/case13_test.go
+++ ArtemisTests/Ginkgo/Login/case13_test.go
@@ -21,9 +21,9 @@ var _ = ginkgo.Describe("Login Credentials Validation Test with AMQP", func() {
         ctx = context.Background()
         // Replace with actual credentials and Artemis server address
         client, err = amqp.Dial("amqp://ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619", amqp.ConnSASLPlain("gfvderethgbfvdrtyhjngbfrtyhfgbdsrtyhgbfvretyhgbfdrtyjhgfr456u7hgfr5678iujyh78iuyhtr6yujhbgfde324rew34refvghnjuy89oiujhgfde3435y6hrgfhnjyuki8uyjhgrfe45t6yujthgbfty5u76i867urthgbdfre4t5y6u7i879ikujyhtrgfvdce32regbhjuki9o0poikujyhtgfvbrhy", "gfkldeority9tr8e372345869oulhgfkdjhfvguhilj,ytrjehdyfgtuiykhjghfregtyr57t689543874ugtriejrtug78fuie3j4hgetwr2435w6yusdifotkj67h65j4ui390w9sd8f7v6ctdgerbntgmfkdsiwq89osdlfgkhyoup90876857436254qwrtdfyhvjcmdkfjuty54uiejfghtryeudjfgnbytgreftydufigoy7i6u5y4t32rqfwsgbnmlqazdfgykiutyr"))
-        if err != nil && strings.Contains(err.Error(), "SASL PLAIN auth failed with code 0x1") {
+        if err != nil && strings.Contains(err.Error(), "frame larger than peer's max frame size") {
             // If the specific error is encountered, consider the test as passed
-            fmt.Println("frame larger than peer's max frame size", err)
+            fmt.Println("[ERROR] Frame size error encountered:", err)
             ginkgo.Skip("Frame size error: aborting test")
         }
 
