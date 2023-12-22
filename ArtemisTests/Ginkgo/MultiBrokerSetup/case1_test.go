93d61fe6a5e8995e063462028d1c08116c186f9c
Laurent <ldassen1998@gmail.com>
2023-12-22 13:37:55 +0100

    Update case1_test.go

--- ArtemisTests/Ginkgo/MultiBrokerSetup/case1_test.go
+++ ArtemisTests/Ginkgo/MultiBrokerSetup/case1_test.go
@@ -1,11 +1,7 @@
 package MultiBrokerSetup_test
 
 import (
-    "context"
-    "fmt"
-    "io/ioutil"
-    "path/filepath"
-    "time"
+    // ... [other imports] ...
 
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -14,7 +10,8 @@ import (
     "k8s.io/client-go/dynamic"
     "k8s.io/client-go/kubernetes"
     "k8s.io/client-go/rest"
-    "k8s.io/apimachinery/pkg/util/wait"
+    "path/filepath"
+    "io/ioutil"
 
     "github.com/onsi/ginkgo/v2"
     "github.com/onsi/gomega"
@@ -47,9 +44,8 @@ var _ = ginkgo.Describe("ActiveMQ Artemis Deployment Test", func() {
         }
     })
 
-    ginkgo.It("Should create ActiveMQArtemis resource and verify pods", func() {
+    ginkgo.It("Should create ActiveMQArtemis resource", func() {
         fileName := "ex-aao.yaml"
-        namespace := "activemq-artemis-brokers"
 
         filePath, err := filepath.Abs(fileName)
         gomega.Expect(err).NotTo(gomega.HaveOccurred())
@@ -65,55 +61,14 @@ var _ = ginkgo.Describe("ActiveMQ Artemis Deployment Test", func() {
         obj.SetAPIVersion("broker.amq.io/v1beta1")
         obj.SetKind("ActiveMQArtemis")
 
-        // Extract the replication factor from the YAML file
-        replicationFactor, found, err := unstructured.NestedInt64(obj.Object, "spec", "deploymentPlan", "size")
-        gomega.Expect(err).NotTo(gomega.HaveOccurred())
-        gomega.Expect(found).To(gomega.BeTrue(), "Replication factor (size) not found in the YAML file")
-        expectedPodCount := int(replicationFactor)
+        resourceClient := dynamicClient.Resource(resourceGVR).Namespace(namespace)
 
-        resourceClient := dynamicClient.Resource(schema.GroupVersionResource{
-            Group:    gvk.Group,    // or "broker.amq.io"
-            Version:  gvk.Version,  // or "v1beta1"
-            Resource: "activemqartemises", // Adjust according to the CRD definition
-        }).Namespace(namespace)
-
-        _, err = resourceClient.Create(context.TODO(), obj, metav1.CreateOptions{})
+        createdObj, err := resourceClient.Create(context.TODO(), obj, metav1.CreateOptions{})
         gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error creating ActiveMQArtemis resource")
 
-        // Wait for the pods to be running
-		labelSelector := fmt.Sprintf("app=%s", obj.GetLabels()["app"])
-		err = wait.PollImmediate(10*time.Second, 3*time.Minute, func() (bool, error) { // Increase the timeout to 5 minutes
-			podList, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
-				LabelSelector: labelSelector,
-			})
-			if err != nil {
-				return false, err
-			}
-			runningPods := 0
-			for _, pod := range podList.Items {
-				if pod.Status.Phase == "Running" {
-					runningPods++
-				}
-			}
-			return runningPods == expectedPodCount, nil
-		})
-		gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error waiting for broker pods to be running")
-	
-        // Delete the ActiveMQArtemis resource
-        err = resourceClient.Delete(context.TODO(), obj.GetName(), metav1.DeleteOptions{})
-        gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error deleting ActiveMQArtemis resource")
+        // Confirm that the resource has been created
+        fmt.Printf("Created ActiveMQArtemis resource: %s\n", createdObj.GetName())
     })
 
-	ginkgo.AfterEach(func() {
-		// Clean up: Delete the ActiveMQArtemis resource
-		if resourceName != "" {
-			fmt.Println("Deleting the ActiveMQArtemis resource:", resourceName)
-			err := dynamicClient.Resource(resourceGVR).Namespace(namespace).Delete(context.TODO(), resourceName, metav1.DeleteOptions{})
-			if err != nil {
-				fmt.Printf("Error deleting ActiveMQArtemis resource: %v\n", err)
-			}
-		}
-	})
-	
-
+    // No AfterEach block needed since we are not deleting the resource
 })
