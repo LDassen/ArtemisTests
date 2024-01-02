4c532e3ed5cd80933a895a455a727e291645a9b6
Sabrine <sabrine7b@hotmail.com>
2024-01-02 12:45:27 +0100

    Update case2_test.go

--- ArtemisTests/Ginkgo/NodeAffinity/case2_test.go
+++ ArtemisTests/Ginkgo/NodeAffinity/case2_test.go
@@ -3,27 +3,22 @@ package NodeAffinity_test
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
-	"path/filepath"
 	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/util/wait"
+	"k8s.io/client-go/util/retry"
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
-	"k8s.io/client-go/dynamic"
+	"k8s.io/apimachinery/pkg/api/errors"
 )
 
 var _ = ginkgo.Describe("ActiveMQ Artemis Node Affinity Test", func() {
 	var kubeClient *kubernetes.Clientset
-	var dynamicClient dynamic.Interface
 	var namespace string
-	var resourceGVR schema.GroupVersionResource
 
 	ginkgo.BeforeEach(func() {
 		var err error
@@ -33,80 +28,93 @@ var _ = ginkgo.Describe("ActiveMQ Artemis Node Affinity Test", func() {
 		kubeClient, err = kubernetes.NewForConfig(config)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-		dynamicClient, err = dynamic.NewForConfig(config)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
 		namespace = "activemq-artemis-brokers"
-		resourceGVR = schema.GroupVersionResource{
-			Group:    "broker.amq.io",
-			Version:  "v1beta1",
-			Resource: "activemqartemises",
-		}
+	})
+
+	ginkgo.AfterEach(func() {
+		// Ensure cleanup after each test
+		_ = deleteActiveMQArtemisResource("ex-aao")
 	})
 
 	ginkgo.It("Should have ActiveMQArtemis pods on at least two different nodes", func() {
-		// Delete the specific instance (CR) of ActiveMQArtemis
-		err := dynamicClient.Resource(resourceGVR).Namespace(namespace).Delete(context.TODO(), "ex-aao", metav1.DeleteOptions{})
-		gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error deleting ActiveMQArtemis resource")
+		// Deploy the ActiveMQArtemis resource without node affinity
+		deployActiveMQArtemisResource("ex-aao.yaml")
 
-		// Deploy the ActiveMQ Artemis broker without node affinity rules
-		fileName := "ex-aaoNA.yaml"
-		deployBroker(fileName, dynamicClient, resourceGVR, namespace)
-
-		// Wait for the broker pods to be created (maximum wait time: 5 minutes)
-		maxWaitTime := 5 * time.Minute
-		startTime := time.Now()
-
-		for {
-			if time.Since(startTime) > maxWaitTime {
-				ginkgo.Fail("Timed out waiting for broker pods on at least two different nodes")
-			}
-
-			// Check if broker pods are on at least two different nodes
-			pods, err := kubeClient.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
-				LabelSelector: "ActiveMQArtemis=ex-aao,application=ex-aao-app",
-			})
-			gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error getting broker pod list")
-
-			// Check if broker pods are on at least two different nodes
-			nodes := make(map[string]struct{})
-			for _, pod := range pods.Items {
-				nodeName := pod.Spec.NodeName
-				nodes[nodeName] = struct{}{}
-			}
-
-			// Confirm that there are at least two different nodes
-			if len(nodes) > 1 {
-				break
-			}
-
-			// Wait for a short interval before checking again
-			time.Sleep(5 * time.Second)
-		}
-
-		fmt.Println("All ActiveMQArtemis broker pods are on different nodes.")
+		// Wait for the deployment to stabilize
+		gomega.Eventually(func() bool {
+			return areActiveMQArtemisPodsOnDifferentNodes("ex-aao")
+		}, time.Minute, time.Second).Should(gomega.BeTrue())
 	})
 })
 
-func deployBroker(fileName string, dynamicClient dynamic.Interface, resourceGVR schema.GroupVersionResource, namespace string) {
+func deleteActiveMQArtemisResource(name string) error {
+	// Get the dynamic client
+	resourceClient := dynamicClient.Resource(resourceGVR).Namespace(namespace)
+
+	// Delete the ActiveMQArtemis resource
+	err := resourceClient.Delete(context.TODO(), name, metav1.DeleteOptions{})
+	if err != nil && !errors.IsNotFound(err) {
+		return err
+	}
+
+	// Ignore NotFound errors and proceed
+	return nil
+}
+
+func deployActiveMQArtemisResource(fileName string) {
+	// Get the absolute path of the YAML file
 	filePath, err := filepath.Abs(fileName)
 	gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
+	// Read the YAML file content
 	fileBytes, err := ioutil.ReadFile(filePath)
 	gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
+	// Decode the YAML content to unstructured object
 	decUnstructured := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
 	obj := &unstructured.Unstructured{}
 	_, _, err = decUnstructured.Decode(fileBytes, nil, obj)
 	gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
+	// Set API version and kind
 	obj.SetAPIVersion("broker.amq.io/v1beta1")
 	obj.SetKind("ActiveMQArtemis")
 
+	// Get the dynamic client
 	resourceClient := dynamicClient.Resource(resourceGVR).Namespace(namespace)
 
+	// Create the ActiveMQArtemis resource
 	createdObj, err := resourceClient.Create(context.TODO(), obj, metav1.CreateOptions{})
 	gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error creating ActiveMQArtemis resource")
 
+	// Confirm that the resource has been created
 	fmt.Printf("Created ActiveMQArtemis resource: %s\n", createdObj.GetName())
+}
+
+func areActiveMQArtemisPodsOnDifferentNodes(name string) bool {
+	// Get the list of broker pods in the namespace
+	pods, err := kubeClient.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
+		LabelSelector: "ActiveMQArtemis=" + name + ",application=" + name + "-app",
+	})
+	gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error getting broker pod list")
+
+	// Print debugging information
+	fmt.Printf("Namespace: %s\n", namespace)
+	fmt.Printf("Found %d broker pods\n", len(pods.Items))
+
+	// Check if broker pods are on different nodes
+	nodes := make(map[string]struct{})
+	for _, pod := range pods.Items {
+		nodeName := pod.Spec.NodeName
+		_, exists := nodes[nodeName]
+		gomega.Expect(exists).To(gomega.BeFalse(), fmt.Sprintf("Broker Pod %s is on the same node as another pod", pod.Name))
+		nodes[nodeName] = struct{}{}
+
+		// Print the pod name and associated node
+		fmt.Printf("Broker Pod Name: %s, Node: %s\n", pod.Name, nodeName)
+	}
+
+	// Confirm that broker pods are on different nodes
+	fmt.Println("All ActiveMQArtemis broker pods are on different nodes.")
+	return len(nodes) > 1
 }
