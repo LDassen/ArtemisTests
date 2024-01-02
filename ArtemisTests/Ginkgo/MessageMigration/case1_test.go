7e695bb76806a0cd15ce97ceca00459587ae34e5
Sabrine <sabrine7b@hotmail.com>
2024-01-02 15:42:57 +0100

    ok

--- ArtemisTests/Ginkgo/MessageMigration/case1_test.go
+++ ArtemisTests/Ginkgo/MessageMigration/case1_test.go
@@ -14,105 +14,17 @@ import (
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/util/homedir"
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 )
 
-
-var kubeClient *kubernetes.Clientset
-
-func init() {
-	gomega.RegisterFailHandler(ginkgo.Fail)
-	format.UseStringRepresentation = false
-}
-
-func NewGomegaWithT(t types.GomegaTestingT) gomega.Gomega {
-	return gomega.NewWithT(t)
-}
-
-// Helper function to wait for pods to be ready
-func waitForPodsToBeReady(namespace, labelSelector string, expectedReplicaCount int) {
-	var podsReady bool
-	var err error
-
-	timeout := time.Now().Add(time.Minute * 5)
-	pollInterval := time.Second * 5
-
-	for time.Now().Before(timeout) {
-		podsReady, err = arePodsReady(namespace, labelSelector, expectedReplicaCount)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error checking if pods are ready")
-
-		if podsReady {
-			break
-		}
-
-		time.Sleep(pollInterval)
-	}
-
-	gomega.Expect(podsReady).To(gomega.BeTrue(), "Pods did not become ready within the timeout period")
-}
-
-// Helper function to check if pods are ready
-func arePodsReady(namespace, labelSelector string, expectedReplicaCount int) (bool, error) {
-	pods, err := kubeClient.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
-		LabelSelector: labelSelector,
-	})
-	if err != nil {
-		return false, err
-	}
-
-	if len(pods.Items) != expectedReplicaCount {
-		return false, nil
-	}
-
-	for _, pod := range pods.Items {
-		if pod.Status.Phase != corev1.PodRunning {
-			return false, nil
-		}
-
-		for _, condition := range pod.Status.Conditions {
-			if condition.Type == corev1.PodReady && condition.Status != corev1.ConditionTrue {
-				return false, nil
-			}
-		}
-	}
-
-	return true, nil
-}
-
-// Helper function to check drainer image logs
-func checkDrainerImageLogs(namespace, labelSelector string) {
-	pods, err := kubeClient.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
-		LabelSelector: labelSelector,
-	})
-	gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error listing pods")
-
-	for _, pod := range pods.Items {
-		// Fetch and check logs
-		logs, err := kubeClient.CoreV1().Pods(namespace).GetLogs(pod.Name, &corev1.PodLogOptions{}).DoRaw(context.TODO())
-		gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error fetching pod logs")
-
-		// TODO: Implement logic to check for the drainer image in logs
-		// For example, you can use regular expressions or any other logic based on your application logs.
-		fmt.Printf("Logs for pod %s:\n%s\n", pod.Name, logs)
-	}
-}
-
-var _ = ginkgo.Describe("ActiveMQ Artemis Deployment Test", func() {
+var _ = ginkgo.Describe("ActiveMQ Artemis Message Migration Test", func() {
 	var dynamicClient dynamic.Interface
+	var kubeClient *kubernetes.Clientset
 	var namespace string
 	var resourceGVR schema.GroupVersionResource
-
-	ginkgo.BeforeSuite(func() {
-		kubeconfigPath := filepath.Join(homeDir(), ".kube", "config")
-		config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-		kubeClient, err = kubernetes.NewForConfig(config)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-	})
 
 	ginkgo.BeforeEach(func() {
 		var err error
@@ -120,6 +32,9 @@ var _ = ginkgo.Describe("ActiveMQ Artemis Deployment Test", func() {
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
 		dynamicClient, err = dynamic.NewForConfig(config)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		kubeClient, err = kubernetes.NewForConfig(config)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
 		namespace = "activemq-artemis-brokers"
@@ -130,9 +45,9 @@ var _ = ginkgo.Describe("ActiveMQ Artemis Deployment Test", func() {
 		}
 	})
 
-	ginkgo.It("Should create ActiveMQArtemis resource", func() {
+	ginkgo.It("Should perform ActiveMQ Artemis Message Migration", func() {
+		// Apply the YAML file to create ActiveMQArtemis resource
 		fileName := "ex-aaoMM.yaml"
-
 		filePath, err := filepath.Abs(fileName)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
@@ -155,10 +70,38 @@ var _ = ginkgo.Describe("ActiveMQ Artemis Deployment Test", func() {
 		// Confirm that the resource has been created
 		fmt.Printf("Created ActiveMQArtemis resource: %s\n", createdObj.GetName())
 
-		// Wait for pods to be ready
-		waitForPodsToBeReady(namespace, "ActiveMQArtemis=ex-aao,application=ex-aao-app", 2)
+		// Wait for some time for the pods to stabilize
+		time.Sleep(2 * time.Minute)
 
-		// Check drainer image logs
-		checkDrainerImageLogs(namespace, "ActiveMQArtemis=ex-aao,application=ex-aao-app")
+		// Check if the starting situation of 3 brokers becomes 2 brokers
+		labelSelector := "ActiveMQArtemis=ex-aao,application=ex-aao-app"
+		expectedReplicaCount := 2
+		podsReady, err := arePodsReady(namespace, labelSelector, expectedReplicaCount)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error checking pod readiness")
+
+		gomega.Expect(podsReady).To(gomega.BeTrue(), "Pods are not ready")
+
+		// TODO: Implement logic to check the logs from the broker that goes down
+		// For example, you can use kubeClient.CoreV1().Pods(namespace).GetLogs() to fetch logs.
+
+		// TODO: Implement logic to check the logs for the drainer pod that gets started and closed
+		// For example, you can use kubeClient.CoreV1().Pods(namespace).GetLogs() to fetch logs.
 	})
 })
+
+func arePodsReady(namespace, labelSelector string, expectedReplicaCount int) (bool, error) {
+	pods, err := kubeClient.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
+		LabelSelector: labelSelector,
+	})
+	if err != nil {
+		return false, err
+	}
+
+	readyCount := 0
+	for _, pod := range pods.Items {
+		if pod.Status.Phase == corev1.PodRunning {
+			readyCount++
+		}
+	}
+
+	return read
