ab6be4e5ca222c4b52dde6040233907044a58d67
Sabrine <sabrine7b@hotmail.com>
2024-01-02 11:42:45 +0100

    Update case2_test.go

--- ArtemisTests/Ginkgo/NodeAffinity/case2_test.go
+++ ArtemisTests/Ginkgo/NodeAffinity/case2_test.go
@@ -3,80 +3,52 @@ package NodeAffinity_test
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
-	"path/filepath"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
-	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 )
 
-var _ = ginkgo.Describe("ActiveMQ Artemis Deployment Test", func() {
-	var dynamicClient dynamic.Interface
+var _ = ginkgo.Describe("ActiveMQ Artemis Node Affinity Test", func() {
+	var kubeClient *kubernetes.Clientset
 	var namespace string
-	var resourceGVR schema.GroupVersionResource
 
 	ginkgo.BeforeEach(func() {
 		var err error
 		config, err := rest.InClusterConfig()
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-		dynamicClient, err = dynamic.NewForConfig(config)
+		kubeClient, err = kubernetes.NewForConfig(config)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
 		namespace = "activemq-artemis-brokers"
-		resourceGVR = schema.GroupVersionResource{
-			Group:    "broker.amq.io",
-			Version:  "v1beta1",
-			Resource: "activemqartemises",
-		}
 	})
 
-	ginkgo.It("Should create ActiveMQArtemis resource", func() {
-		fileName := "ex-aao.yaml"
+	ginkgo.It("Should have ActiveMQArtemis pods on at least two different nodes", func() {
+		// Get the list of broker pods in the namespace
+		pods, err := kubeClient.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
+			LabelSelector: "ActiveMQArtemis=ex-aao,application=ex-aao-app", // Update with the actual label selector for broker pods
+		})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error getting broker pod list")
 
-		filePath, err := filepath.Abs(fileName)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		// Print debugging information
+		fmt.Printf("Namespace: %s\n", namespace)
+		fmt.Printf("Found %d broker pods\n", len(pods.Items))
 
-		fileBytes, err := ioutil.ReadFile(filePath)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-		decUnstructured := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
-		obj := &unstructured.Unstructured{}
-		_, _, err = decUnstructured.Decode(fileBytes, nil, obj)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-		obj.SetAPIVersion("broker.amq.io/v1beta1")
-		obj.SetKind("ActiveMQArtemis")
-
-		resourceClient := dynamicClient.Resource(resourceGVR).Namespace(namespace)
-
-		createdObj, err := resourceClient.Create(context.TODO(), obj, metav1.CreateOptions{})
-		gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error creating ActiveMQArtemis resource")
-
-		// Confirm that the resource has been created
-		fmt.Printf("Created ActiveMQArtemis resource: %s\n", createdObj.GetName())
-
-		// Check if pods are on different nodes
-		pods, err := dynamicClient.Resource(schema.GroupVersionResource{
-			Version:  "v1",
-			Resource: "pods",
-		}).Namespace(namespace).List(context.TODO(), metav1.ListOptions{})
-		gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error listing pods")
-
-		// Check if pods are on at least two different nodes
+		// Check if broker pods are on at least two different nodes
 		nodes := make(map[string]struct{})
 		for _, pod := range pods.Items {
 			nodeName := pod.Spec.NodeName
 			nodes[nodeName] = struct{}{}
+
+			// Print the pod name and associated node
+			fmt.Printf("Broker Pod Name: %s, Node: %s\n", pod.Name, nodeName)
 		}
 
-		gomega.Expect(len(nodes)).To(gomega.BeNumerically(">=", 2), "Pods are not deployed on at least two nodes")
+		// Confirm that there are at least two different nodes
+		gomega.Expect(len(nodes)).To(gomega.BeNumerically(">", 1), "Expected broker pods on at least two different nodes")
 	})
 })
