195b0f1dcabb74d7fe04d6a44f37ffbf907e44a9
Sabrine <sabrine7b@hotmail.com>
2024-01-03 14:27:58 +0100

    sd

--- ArtemisTests/Ginkgo/MessageMigration/case1_test.go
+++ ArtemisTests/Ginkgo/MessageMigration/case1_test.go
@@ -3,97 +3,88 @@ package MessageMigration_test
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
-	"path/filepath"
-	"reflect"
+	"testing"
+	"time"
 
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-
-	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"pack.ag/amqp"
 )
 
-var _ = ginkgo.Describe("ActiveMQ Artemis Deployment Test", func() {
-	var clientset *kubernetes.Clientset
+var _ = ginkgo.Describe("Queue Sender Test", func() {
+	var client *amqp.Client
+	var session *amqp.Session
+	var sender *amqp.Sender
+	var ctx context.Context
+	var err error
+
+	var kubeClient *kubernetes.Clientset
+	var podName string
 	var namespace string
-	var resourceGVR schema.GroupVersionResource
 
 	ginkgo.BeforeEach(func() {
-		var err error
+		ctx = context.Background()
 
-		config, err := rest.InClusterConfig()
+		// Establish connection to the Artemis broker
+		client, err = amqp.Dial("amqp://ex-aao-ss-2.activemq-artemis-brokers.svc.cluster.local:61619", amqp.ConnSASLPlain("cgi", "cgi"))
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		session, err = client.NewSession()
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-		clientset, err = kubernetes.NewForConfig(config)
+		// Initialize Kubernetes client
+		kubeConfigPath := "/path/to/your/kube/config"
+		config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		kubeClient, err = kubernetes.NewForConfig(config)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-		namespace = "activemq-artemis-brokers"
-		resourceGVR = schema.GroupVersionResource{
-			Group:    "broker.amq.io",
-			Version:  "v1beta1",
-			Resource: "activemqartemises",
+		// Set your pod name and namespace
+		podName = "ex-aao-ss-2"
+		namespace = "your-namespace"
+	})
+
+	ginkgo.It("should send messages to a specific queue in a pod", func() {
+		queueName := "Sabrine"
+		messageText := "Hello, this is a test message"
+
+		// Get the pod
+		pod, err := kubeClient.CoreV1().Pods(namespace).Get(ctx, podName, metav1.GetOptions{})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		// Create a sender
+		sender, err = session.NewSender(
+			amqp.LinkTargetAddress(queueName),
+		)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		// Send messages to the specific queue in the pod
+		for i := 0; i < 3; i++ {
+			err = sender.Send(ctx, amqp.NewMessage([]byte(fmt.Sprintf("%s %d", messageText, i+1))))
+			gomega.Expect(err).NotTo(gomega.HaveOccurred()
+			
+			// Wait for a short duration between messages
+			time.Sleep(1 * time.Second)
 		}
 	})
 
-	ginkgo.It("Should create or update ActiveMQArtemis resource", func() {
-		fileName := "ex-aaoMM.yaml"
-
-		filePath, err := filepath.Abs(fileName)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-		fileBytes, err := ioutil.ReadFile(filePath)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-		decUnstructured := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
-		obj := &unstructured.Unstructured{}
-		_, _, err = decUnstructured.Decode(fileBytes, nil, obj)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-		obj.SetAPIVersion("broker.amq.io/v1beta1")
-		obj.SetKind("ActiveMQArtemis")
-
-		resourceClient := clientset.Dynamic().Resource(resourceGVR).Namespace(namespace)
-
-		// Try to get the existing resource
-		existingObj, err := resourceClient.Get(context.TODO(), obj.GetName(), metav1.GetOptions{})
-		if err == nil {
-			// Resource already exists, update it if needed
-			if !reflect.DeepEqual(existingObj.Object, obj.Object) {
-				fmt.Printf("ActiveMQArtemis resource already exists, updating configuration.\n")
-
-				// Set the UID and ResourceVersion to perform an update
-				obj.SetUID(existingObj.GetUID())
-				obj.SetResourceVersion(existingObj.GetResourceVersion())
-
-				_, err = resourceClient.Update(context.TODO(), obj, metav1.UpdateOptions{})
-				gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error updating ActiveMQArtemis resource")
-			} else {
-				fmt.Printf("ActiveMQArtemis resource already exists and has the same configuration.\n")
-			}
-			return
+	ginkgo.AfterEach(func() {
+		if sender != nil {
+			sender.Close(ctx)
 		}
-
-		// If the resource does not exist, create it
-		createdObj, err := resourceClient.Create(context.TODO(), obj, metav1.CreateOptions{})
-		gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Error creating ActiveMQArtemis resource")
-
-		// Confirm that the resource has been created
-		fmt.Printf("Created ActiveMQArtemis resource: %s\n", createdObj.GetName())
-	})
-
-	ginkgo.It("Should retrieve logs from ex-aao-ss-2 pod", func() {
-		podName := "ex-aao-ss-2"
-		namespace := "activemq-artemis-brokers"
-
-		logs, err := clientset.CoreV1().Pods(namespace).GetLogs(podName, &corev1.PodLogOptions{}).DoRaw(context.TODO())
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-		fmt.Printf("Logs from %s pod:\n%s\n", podName, logs)
+		if session != nil {
+			session.Close(ctx)
+		}
+		if client != nil {
+			client.Close()
+		}
 	})
 })
+
+func TestQueueSender(t *testing.T) {
+	gomega.RegisterFailHandler(ginkgo.Fail)
+	ginkgo.RunSpecs(t, "MessageMigration Test Suite")
+}
