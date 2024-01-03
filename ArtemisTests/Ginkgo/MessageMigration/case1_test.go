a154ab8007127fae65f29382374d0b31efb0b051
Sabrine <sabrine7b@hotmail.com>
2024-01-03 15:35:22 +0100

    Update case1_test.go

--- ArtemisTests/Ginkgo/MessageMigration/case1_test.go
+++ ArtemisTests/Ginkgo/MessageMigration/case1_test.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
-	v1 "k8s.io/api/core/v1"
+	//v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
