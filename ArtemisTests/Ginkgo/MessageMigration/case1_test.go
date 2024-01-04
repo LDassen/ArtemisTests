2adffd3c278a50f3bc2b1fa3006f2141219b4405
Sabrine <sabrine7b@hotmail.com>
2024-01-04 10:56:34 +0100

    Update case1_test.go

--- ArtemisTests/Ginkgo/MessageMigration/case1_test.go
+++ ArtemisTests/Ginkgo/MessageMigration/case1_test.go
@@ -8,9 +8,10 @@ import (
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 	"pack.ag/amqp"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = ginkgo.Describe("MessageMigration Test", func() {
@@ -27,28 +28,18 @@ var _ = ginkgo.Describe("MessageMigration Test", func() {
 	ginkgo.BeforeEach(func() {
 		ctx = context.Background()
 
-		fmt.Println("Connecting to the Artemis broker...")
 		// Establish connection to the Artemis broker
 		client, err = amqp.Dial(
 			"amqp://ex-aao-ss-2.ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619",
 			amqp.ConnSASLPlain("cgi", "cgi"),
 			amqp.ConnIdleTimeout(30*time.Second),
 		)
-		if err != nil {
-			fmt.Println("Error during connection:", err)
-			return
-		}
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 		fmt.Println("Connected successfully.")
 
-		fmt.Println("Creating a session...")
 		// Create a session
 		session, err = client.NewSession()
-		if err != nil {
-			fmt.Println("Error during session creation:", err)
-			// Close the client on error
-			client.Close()
-			return
-		}
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 		fmt.Println("Session created successfully.")
 
 		// Initialize Kubernetes client with in-cluster config
@@ -59,14 +50,6 @@ var _ = ginkgo.Describe("MessageMigration Test", func() {
 
 		// Set the namespace
 		namespace = "activemq-artemis-brokers"
-
-		// Ensure the StatefulSet (deployment) exists before proceeding
-		statefulSetName := "ex-aao-ss"
-		_, err = kubeClient.AppsV1().StatefulSets(namespace).Get(ctx, statefulSetName, metav1.GetOptions{})
-		if err != nil {
-			fmt.Printf("Error getting StatefulSet %s: %v\n", statefulSetName, err)
-			return
-		}
 	})
 
 	ginkgo.It("should send, delete, and check messages", func() {
@@ -84,12 +67,24 @@ var _ = ginkgo.Describe("MessageMigration Test", func() {
 		// Wait for a short duration
 		time.Sleep(1 * time.Second)
 
-		// Delete the ex-aao-ss-2 broker
-		err = kubeClient.AppsV1().StatefulSets(namespace).Delete(ctx, "ex-aao-ss-2", metav1.DeleteOptions{})
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		// List pods with the label selector "application=ex-aao-app"
+		pods, err := kubeClient.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{LabelSelector: "application=ex-aao-app"})
+		gomega.Expect(err).To(BeNil(), "Error getting pods: %v", err)
 
-		// Wait for the deletion to propagate
-		time.Sleep(30 * time.Second)
+		// Check if there are any pods with the specified label
+		if len(pods.Items) > 0 {
+			// Get the name of the last pod in the list
+			lastPodName := pods.Items[len(pods.Items)-1].Name
+
+			// Delete the last pod
+			err := kubeClient.CoreV1().Pods(namespace).Delete(ctx, lastPodName, metav1.DeleteOptions{})
+			gomega.Expect(err).To(BeNil(), "Error deleting pod: %v", err)
+
+			// Wait for the deletion to propagate
+			time.Sleep(30 * time.Second)
+		} else {
+			fmt.Println("No pods found with label 'application=ex-aao-app'")
+		}
 
 		// Loop through the pod names (ex-aao-ss-0, ex-aao-ss-1) to find the specific message
 		for range []string{"ex-aao-ss-0", "ex-aao-ss-1"} {
