4481400cb799486ab8399f0ae357098da028243a
Laurent <ldassen1998@gmail.com>
2024-01-04 14:41:45 +0100

    s

--- ArtemisTests/Ginkgo/Login/case13_test.go
+++ ArtemisTests/Ginkgo/Login/case13_test.go
@@ -2,8 +2,8 @@ package Login_test
 
 import (
     "context"
-    "fmt"
     "strings"
+    "fmt"
     "github.com/onsi/ginkgo/v2"
     "github.com/onsi/gomega"
     "pack.ag/amqp" // AMQP library for Go
@@ -12,6 +12,8 @@ import (
 var _ = ginkgo.Describe("Login Credentials Validation Test with AMQP", func() {
     var client *amqp.Client
     var session *amqp.Session
+    var sender *amqp.Sender
+    var receiver *amqp.Receiver
     var ctx context.Context
     var err error
 
@@ -19,29 +21,61 @@ var _ = ginkgo.Describe("Login Credentials Validation Test with AMQP", func() {
         ctx = context.Background()
         // Replace with actual credentials and Artemis server address
         client, err = amqp.Dial("amqp://ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619", amqp.ConnSASLPlain("gfvderethgbfvdrtyhjngbfrtyhfgbdsrtyhgbfvretyhgbfdrtyjhgfr456u7hgfr5678iujyh78iuyhtr6yujhbgfde324rew34refvghnjuy89oiujhgfde3435y6hrgfhnjyuki8uyjhgrfe45t6yujthgbfty5u76i867urthgbdfre4t5y6u7i879ikujyhtrgfvdce32regbhjuki9o0poikujyhtgfvbrhy", "gfkldeority9tr8e372345869oulhgfkdjhfvguhilj,ytrjehdyfgtuiykhjghfregtyr57t689543874ugtriejrtug78fuie3j4hgetwr2435w6yusdifotkj67h65j4ui390w9sd8f7v6ctdgerbntgmfkdsiwq89osdlfgkhyoup90876857436254qwrtdfyhvjcmdkfjuty54uiejfghtryeudjfgnbytgreftydufigoy7i6u5y4t32rqfwsgbnmlqazdfgykiutyr"))
-
-        if err != nil && strings.Contains(err.Error(), "SASL PLAIN auth failed with code 0x1") {
+        if err != nil && strings.Contains(err.Error(), "frame larger than peer's max frame size") {
             // If the specific error is encountered, consider the test as passed
-            fmt.Println("[PASSED] Expected error encountered:", err)
-            ginkgo.Skip("Skipping the rest of the test due to expected error.")
-        } else if err == nil {
-            ginkgo.Fail("Login should not be successful with empty credentials")
+            fmt.Println("[ERROR] Frame size error encountered:", err)
+            ginkgo.Skip("Frame size error: aborting test")
         }
 
-        gomega.Expect(err).To(gomega.HaveOccurred()) // Fail the test if login is successful
+        gomega.Expect(err).To(gomega.HaveOccurred())
         session, err = client.NewSession()
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
     })
 
-    ginkgo.It("should not be able to send and receive a message due to invalid login credentials", func() {
-        gomega.Expect(client).To(gomega.BeNil()) // The client should be nil as the login was not successful
+    ginkgo.It("should send and receive a message, validating the login credentials", func() {
+        queueName := "LOGINQUEUE"
+        messageText := "Test message for login validation"
+
+        // Create a sender
+        sender, err = session.NewSender(
+            amqp.LinkTargetAddress(queueName),
+        )
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+        // Send a message
+        err = sender.Send(ctx, amqp.NewMessage([]byte(messageText)))
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+        sender.Close(ctx)
+
+        // Create a receiver
+        receiver, err = session.NewReceiver(
+            amqp.LinkSourceAddress(queueName),
+        )
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+        // Receive the message
+        msg, err := receiver.Receive(ctx)
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+        gomega.Expect(string(msg.GetData())).To(gomega.Equal(messageText))
+        msg.Accept()
+        receiver.Close(ctx)
+        
+        // Print success message
+        fmt.Println("Login is not successful")
     })
 
     ginkgo.AfterEach(func() {
-        if client != nil {
-            client.Close()
+        if sender != nil {
+            sender.Close(ctx)
+        }
+        if receiver != nil {
+            receiver.Close(ctx)
         }
         if session != nil {
             session.Close(ctx)
+        }
+        if client != nil {
+            client.Close()
         }
     })
 })
