fd1a39229d9f8fbc10f0c9afb05ecf2db89d7586
Laurent <ldassen1998@gmail.com>
2024-01-04 14:25:16 +0100

    e

--- ArtemisTests/Ginkgo/Login/case10_test.go
+++ ArtemisTests/Ginkgo/Login/case10_test.go
@@ -2,9 +2,9 @@ package Login_test
 
 import (
     "context"
-    "github.com/onsi/ginkgo/v2"
     "fmt"
     "strings"
+    "github.com/onsi/ginkgo/v2"
     "github.com/onsi/gomega"
     "pack.ag/amqp" // AMQP library for Go
 )
@@ -12,8 +12,6 @@ import (
 var _ = ginkgo.Describe("Login Credentials Validation Test with AMQP", func() {
     var client *amqp.Client
     var session *amqp.Session
-    var sender *amqp.Sender
-    var receiver *amqp.Receiver
     var ctx context.Context
     var err error
 
@@ -21,61 +19,29 @@ var _ = ginkgo.Describe("Login Credentials Validation Test with AMQP", func() {
         ctx = context.Background()
         // Replace with actual credentials and Artemis server address
         client, err = amqp.Dial("amqp://ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619", amqp.ConnSASLPlain("c g i", "c g i"))
+
         if err != nil && strings.Contains(err.Error(), "SASL PLAIN auth failed with code 0x1") {
             // If the specific error is encountered, consider the test as passed
             fmt.Println("[PASSED] Expected error encountered:", err)
             ginkgo.Skip("Skipping the rest of the test due to expected error.")
+        } else if err == nil {
+            ginkgo.Fail("Login should not be successful with empty credentials")
         }
 
-        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+        gomega.Expect(err).To(gomega.HaveOccurred()) // Fail the test if login is successful
         session, err = client.NewSession()
-        gomega.Expect(err).NotTo(gomega.HaveOccurred())
     })
 
-    ginkgo.It("should send and receive a message, validating the login credentials", func() {
-        queueName := "LOGINQUEUE"
-        messageText := "Test message for login validation"
-
-        // Create a sender
-        sender, err = session.NewSender(
-            amqp.LinkTargetAddress(queueName),
-        )
-        gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-        // Send a message
-        err = sender.Send(ctx, amqp.NewMessage([]byte(messageText)))
-        gomega.Expect(err).NotTo(gomega.HaveOccurred())
-        sender.Close(ctx)
-
-        // Create a receiver
-        receiver, err = session.NewReceiver(
-            amqp.LinkSourceAddress(queueName),
-        )
-        gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-        // Receive the message
-        msg, err := receiver.Receive(ctx)
-        gomega.Expect(err).NotTo(gomega.HaveOccurred())
-        gomega.Expect(string(msg.GetData())).To(gomega.Equal(messageText))
-        msg.Accept()
-        receiver.Close(ctx)
-        
-        // Print success message
-        fmt.Println("Login is not successful")
+    ginkgo.It("should not be able to send and receive a message due to invalid login credentials", func() {
+        gomega.Expect(client).To(gomega.BeNil()) // The client should be nil as the login was not successful
     })
 
     ginkgo.AfterEach(func() {
-        if sender != nil {
-            sender.Close(ctx)
-        }
-        if receiver != nil {
-            receiver.Close(ctx)
+        if client != nil {
+            client.Close()
         }
         if session != nil {
             session.Close(ctx)
-        }
-        if client != nil {
-            client.Close()
         }
     })
 })
