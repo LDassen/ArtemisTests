acba9a7fa89716e9843c360bbdff9aa1212d8485
Laurent <ldassen1998@gmail.com>
2024-01-04 13:29:44 +0100

    Create case2_test.go

---
+++ ArtemisTests/Ginkgo/Security/SSL/case2_test.go
@@ -0,0 +1,34 @@
+package SSL_test
+
+import (
+    . "github.com/onsi/ginkgo/v2"
+    . "github.com/onsi/gomega"
+    "k8s.io/client-go/kubernetes"
+    "k8s.io/client-go/rest"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = Describe("ConfigMap Check in All Namespaces", func() {
+    var clientset *kubernetes.Clientset
+
+    BeforeEach(func() {
+        // Set up the Kubernetes client
+        config, err := rest.InClusterConfig()
+        Expect(err).NotTo(HaveOccurred())
+
+        clientset, err = kubernetes.NewForConfig(config)
+        Expect(err).NotTo(HaveOccurred())
+    })
+
+    It("should have 'ca-bundle' ConfigMap in all namespaces", func() {
+        // Get all namespaces
+        namespaces, err := clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+        Expect(err).NotTo(HaveOccurred())
+
+        for _, namespace := range namespaces.Items {
+            // Check for the presence of 'ca-bundle' ConfigMap in each namespace
+            _, err := clientset.CoreV1().ConfigMaps(namespace.Name).Get(context.TODO(), "ca-bundle", metav1.GetOptions{})
+            Expect(err).NotTo(HaveOccurred(), "Namespace: "+namespace.Name)
+        }
+    })
+})
