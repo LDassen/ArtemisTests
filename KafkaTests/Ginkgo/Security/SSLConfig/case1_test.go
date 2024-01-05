e7e7ab4cd16e05b887f890294b6d7c11d27a7e6f
Laurent <ldassen1998@gmail.com>
2024-01-05 16:31:26 +0100

    Update case1_test.go

--- KafkaTests/Ginkgo/Security/SSLConfig/case1_test.go
+++ KafkaTests/Ginkgo/Security/SSLConfig/case1_test.go
@@ -1,8 +1,15 @@
 package SSLConfig_test
 
 import (
-    // ... other imports ...
+package SSLConfig_test
+
+import (
+    . "github.com/onsi/ginkgo/v2"
+    . "github.com/onsi/gomega"
     "github.com/IBM/sarama"
+    "crypto/tls"
+    "crypto/x509"
+    "io/ioutil"
     "fmt"
     "time"
 )
@@ -44,5 +51,68 @@ var _ = Describe("Kafka SSL Test", func() {
         case <-time.After(30 * time.Second): // Timeout after 30 seconds
             Fail("Timeout while consuming message")
         }
+    })
+})
+
+)
+
+var _ = Describe("Kafka SSL Test", func() {
+    var headless string
+    var topic string
+    var tlsConfig *tls.Config
+
+    BeforeEach(func() {
+        // Load the CA certificate
+        caCertBytes, err := ioutil.ReadFile("/var/kafka/ca.crt") // CA certificate
+        Expect(err).NotTo(HaveOccurred())
+
+        // Create a CA certificate pool
+        caCertPool := x509.NewCertPool()
+        caCertPool.AppendCertsFromPEM(caCertBytes)
+
+        // Load the client certificate and key
+        clientCert, err := tls.LoadX509KeyPair("/var/kafka/tls.crt", "/var/kafka/tls.key")
+        Expect(err).NotTo(HaveOccurred())
+
+        // Create TLS configuration
+        tlsConfig = &tls.Config{
+            RootCAs:      caCertPool,
+            Certificates: []tls.Certificate{clientCert},
+        }
+
+        headless = "kafka-brokers-headless.kafka-brokers.svc.cluster.local:9094"
+        topic = "TESTKUBE"
+    })
+
+    It("should successfully produce and consume messages with Kafka over SSL", func() {
+        config := sarama.NewConfig()
+        config.Net.TLS.Enable = true
+        config.Net.TLS.Config = tlsConfig
+        config.Producer.Return.Successes = true
+
+        // Create a new Sarama producer
+        producer, err := sarama.NewSyncProducer([]string{headless}, config)
+        Expect(err).NotTo(HaveOccurred())
+        defer producer.Close()
+
+        // Produce a message
+        _, _, err = producer.SendMessage(&sarama.ProducerMessage{
+            Topic: topic,
+            Value: sarama.StringEncoder("Hello Kafka over SSL!"),
+        })
+        Expect(err).NotTo(HaveOccurred())
+
+        // Create a new Sarama consumer
+        consumer, err := sarama.NewConsumer([]string{headless}, config)
+        Expect(err).NotTo(HaveOccurred())
+        defer consumer.Close()
+
+        partitionConsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetNewest)
+        Expect(err).NotTo(HaveOccurred())
+        defer partitionConsumer.Close()
+
+        // Consume a message
+        consumedMessage := <-partitionConsumer.Messages()
+        Expect(string(consumedMessage.Value)).To(Equal("Hello Kafka over SSL!"))
     })
 })
