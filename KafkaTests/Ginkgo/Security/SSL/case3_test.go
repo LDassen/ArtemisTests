c13f1d659314821f9652f9ecd4b0eee6394ad74e
Laurent <ldassen1998@gmail.com>
2024-01-05 17:52:09 +0100

    Update case3_test.go

--- KafkaTests/Ginkgo/Security/SSL/case3_test.go
+++ KafkaTests/Ginkgo/Security/SSL/case3_test.go
@@ -1,40 +1,61 @@
-package SSL_test
+package SSLConfig_test
 
 import (
-	"fmt"
-	"testing"
-
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"log"
+	"time"
+	"github.com/IBM/sarama"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/IBM/sarama"
 )
-
-func TestKafkaSSLConnection(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Kafka SSL Connection Suite")
-}
 
 var _ = Describe("Kafka SSL Connection", func() {
 	Context("when sending and receiving messages over SSL", func() {
-		It("should fail to produce and consume messages on TESTKUBE topic", func() {
-			config := sarama.NewConfig()
-			config.Producer.Return.Successes = true
+		It("should not successfully produce and consume messages on TESTKUBE topic", func() {
 
 			broker := "kafka-brokers-headless.kafka-brokers.svc.cluster.local:9094"
 
-			// Attempt to produce a message
+			// Producing a message
 			producer, err := sarama.NewSyncProducer([]string{broker}, config)
-			Expect(err).To(HaveOccurred())
+			Expect(err).NotTo(HaveOccurred())
+			defer func() {
+				if err := producer.Close(); err != nil {
+					log.Println("Error closing producer:", err)
+				}
+			}()
 
 			message := "Hello, TestKube!"
-			_, _, err = producer.SendMessage(&sarama.ProducerMessage{
+			partition, offset, err := producer.SendMessage(&sarama.ProducerMessage{
 				Topic: "TESTKUBE",
 				Value: sarama.StringEncoder(message),
 			})
 			Expect(err).To(HaveOccurred())
 
-			// Always report success even if the test fails
-			fmt.Println("Test succeeded!")
+			// Consuming the message
+			consumer, err := sarama.NewConsumer([]string{broker}, config)
+			Expect(err).To(HaveOccurred())
+			defer func() {
+				if err := consumer.Close(); err != nil {
+					log.Println("Error closing consumer:", err)
+				}
+			}()
+
+			partitionConsumer, err := consumer.ConsumePartition("TESTKUBE", partition, offset)
+			Expect(err).To(HaveOccurred())
+			defer func() {
+				if err := partitionConsumer.Close(); err != nil {
+					log.Println("Error closing partition consumer:", err)
+				}
+			}()
+
+			select {
+			case msg := <-partitionConsumer.Messages():
+				Expect(string(msg.Value)).To(Equal(message))
+			case <-time.After(5 * time.Second):
+				Fail("Timed out waiting for message")
+			}
 		})
 	})
 })
