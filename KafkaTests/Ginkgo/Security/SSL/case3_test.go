a8c6de7cf0453c8ca65cac404ec5620ee4b3e3bd
Laurent <ldassen1998@gmail.com>
2024-01-05 17:47:21 +0100

    Update case3_test.go

--- KafkaTests/Ginkgo/Security/SSL/case3_test.go
+++ KafkaTests/Ginkgo/Security/SSL/case3_test.go
@@ -1,12 +1,21 @@
 package SSL_test
 
 import (
-	"github.com/onsi/ginkgo/v2"
+	"fmt"
+	"log"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/IBM/sarama"
 )
 
-var _ = ginkgo.Describe("Kafka SSL Connection", func() {
+func TestKafkaSSLConnection(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Kafka SSL Connection Suite")
+}
+
+var _ = Describe("Kafka SSL Connection", func() {
 	Context("when sending and receiving messages over SSL", func() {
 		It("should fail to produce and consume messages on TESTKUBE topic", func() {
 			config := sarama.NewConfig()
@@ -16,12 +25,7 @@ var _ = ginkgo.Describe("Kafka SSL Connection", func() {
 
 			// Attempt to produce a message
 			producer, err := sarama.NewSyncProducer([]string{broker}, config)
-			Expect(err).NotTo(HaveOccurred())
-			defer func() {
-				if err := producer.Close(); err != nil {
-					log.Println("Error closing producer:", err)
-				}
-			}()
+			Expect(err).To(HaveOccurred())
 
 			message := "Hello, TestKube!"
 			_, _, err = producer.SendMessage(&sarama.ProducerMessage{
@@ -32,7 +36,7 @@ var _ = ginkgo.Describe("Kafka SSL Connection", func() {
 
 			// Attempt to consume the message
 			consumer, err := sarama.NewConsumer([]string{broker}, config)
-			Expect(err).NotTo(HaveOccurred())
+			Expect(err).To(HaveOccurred())
 			defer func() {
 				if err := consumer.Close(); err != nil {
 					log.Println("Error closing consumer:", err)
@@ -41,9 +45,9 @@ var _ = ginkgo.Describe("Kafka SSL Connection", func() {
 
 			partitionConsumer, err := consumer.ConsumePartition("TESTKUBE", 0, sarama.OffsetNewest)
 			Expect(err).To(HaveOccurred())
-			
+
 			// Always report success even if the test fails
-			By("Test succeeded!")
+			fmt.Println("Test succeeded!")
 		})
 	})
 })
