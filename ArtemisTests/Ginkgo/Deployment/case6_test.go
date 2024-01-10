7c69b6ae38d9bb712db2e90c5cba5054f2890e57
Sabrine <sabrine7b@hotmail.com>
2024-01-10 13:11:18 +0100

    Update case6_test.go

--- ArtemisTests/Ginkgo/Deployment/case6_test.go
+++ ArtemisTests/Ginkgo/Deployment/case6_test.go
@@ -10,22 +10,21 @@ import (
 )
 
 var _ = Describe("Check if ca-bundle ConfigMap is synced", func() {
-	It("should ensure ca-bundle ConfigMap is synced in the cert-manager namespace", func() {
+	It("should ensure ca-bundle ConfigMap is synced", func() {
 		config, err := rest.InClusterConfig()
 		Expect(err).To(BeNil(), "Error getting in-cluster config: %v", err)
 
 		clientset, err := kubernetes.NewForConfig(config)
 		Expect(err).To(BeNil(), "Error creating Kubernetes client: %v", err)
 
-		namespace := "cert-manager"
 		configMapName := "ca-bundle"
 
-		configMap, err := clientset.CoreV1().ConfigMaps(namespace).Get(context.TODO(), configMapName, metav1.GetOptions{})
-		Expect(err).To(BeNil(), "Error getting ConfigMap '%s' in namespace '%s': %v", configMapName, namespace, err)
+		configMap, err := clientset.CoreV1().ConfigMaps("").Get(context.TODO(), configMapName, metav1.GetOptions{})
+		Expect(err).To(BeNil(), "Error getting ConfigMap '%s': %v", configMapName, err)
 
 		// Check if 'SYNCED' field exists and is set to 'True'
 		synced, found := configMap.Data["SYNCED"]
-		Expect(found).To(BeTrue(), "Field 'SYNCED' not found in ConfigMap '%s' in namespace '%s'", configMapName, namespace)
-		Expect(synced).To(Equal("True"), "Expected 'SYNCED' to be 'True' in ConfigMap '%s' in namespace '%s', but found '%s'", configMapName, namespace, synced)
+		Expect(found).To(BeTrue(), "Field 'SYNCED' not found in ConfigMap '%s'", configMapName)
+		Expect(synced).To(Equal("True"), "Expected 'SYNCED' to be 'True' in ConfigMap '%s', but found '%s'", configMapName, synced)
 	})
 })
