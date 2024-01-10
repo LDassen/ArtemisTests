d66919eeb8c0f0aec0a81c955af67c282b5805aa
Sabrine <sabrine7b@hotmail.com>
2024-01-10 16:36:05 +0100

    Update case12_test.go

--- ArtemisTests/Ginkgo/Deployment/case12_test.go
+++ ArtemisTests/Ginkgo/Deployment/case12_test.go
@@ -4,7 +4,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+"k8s.io/client-go/rest"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"context"
 )
@@ -23,14 +23,14 @@ var _ = Describe("ClusterIssuers Check", func() {
 
 	It("should find 'amq-ca-issuer' and 'amq-selfsigned-cluster-issuer'", func() {
 		// Check 'amq-ca-issuer'
-		issuer1, err := clientset.CertmanagerV1().ClusterIssuers().Get(context.TODO(), "amq-ca-issuer", metav1.GetOptions{})
+		issuer1, err := clientset.CertificatesV1().ClusterIssuers().Get(context.TODO(), "amq-ca-issuer", metav1.GetOptions{})
 		Expect(err).NotTo(HaveOccurred(), "Error while checking 'amq-ca-issuer'")
 		Expect(issuer1.Spec.Acme).To(BeNil()) // Assuming you are not using ACME, adjust as needed
 		Expect(issuer1.Status.Conditions[0].Type).To(Equal("Ready"))
 		Expect(issuer1.Status.Conditions[0].Status).To(Equal("True"))
 
 		// Check 'amq-selfsigned-cluster-issuer'
-		issuer2, err := clientset.CertmanagerV1().ClusterIssuers().Get(context.TODO(), "amq-selfsigned-cluster-issuer", metav1.GetOptions{})
+		issuer2, err := clientset.CertificatesV1().ClusterIssuers().Get(context.TODO(), "amq-selfsigned-cluster-issuer", metav1.GetOptions{})
 		Expect(err).NotTo(HaveOccurred(), "Error while checking 'amq-selfsigned-cluster-issuer'")
 		Expect(issuer2.Spec.Acme).To(BeNil()) // Assuming you are not using ACME, adjust as needed
 		Expect(issuer2.Status.Conditions[0].Type).To(Equal("Ready"))
