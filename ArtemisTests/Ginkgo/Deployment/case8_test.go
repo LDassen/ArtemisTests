78d41536bb4e42681eb442aa28c1f32c068196d4
Sabrine <sabrine7b@hotmail.com>
2024-01-10 14:42:14 +0100

    x

---
+++ ArtemisTests/Ginkgo/Deployment/case8_test.go
@@ -0,0 +1,38 @@
+package Deployment_test
+
+import (
+	"context"
+	"fmt"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = Describe("Check the Trust Manager Pod", func() {
+	It("should find and verify the 'trust-manager-*' pod in the 'cert-manager' namespace", func() {
+		config, err := rest.InClusterConfig()
+		Expect(err).To(BeNil(), "Error getting in-cluster config: %v", err)
+
+		clientset, err := kubernetes.NewForConfig(config)
+		Expect(err).To(BeNil(), "Error creating Kubernetes client: %v", err)
+
+		namespace := "cert-manager"
+		expectedPodNamePrefix := "trust-manager-"
+
+		pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+		Expect(err).To(BeNil(), "Error getting pods: %v", err)
+
+		var trustManagerPodFound bool
+		for _, pod := range pods.Items {
+			if strings.HasPrefix(pod.Name, expectedPodNamePrefix) && pod.Status.Phase == "Running" {
+				fmt.Printf("Trust Manager Pod Name: %s\n", pod.Name)
+				trustManagerPodFound = true
+				break
+			}
+		}
+
+		Expect(trustManagerPodFound).To(BeTrue(), "Trust Manager pod not found or not running in namespace '%s'", namespace)
+	})
+})
