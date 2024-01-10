8871aef57f491f419c83be7610a1573c53f83977
Laurent <ldassen1998@gmail.com>
2024-01-10 10:46:23 +0100

    d

---
+++ ArtemisTests/Ginkgo/RoshanTest/autocreationqueue_test.go
@@ -0,0 +1,95 @@
+package RoshanTest_test
+
+import (
+    "context"
+    "github.com/onsi/ginkgo/v2"
+    "github.com/onsi/gomega"
+    "pack.ag/amqp" // AMQP library for Go
+	"time"
+)
+
+var _ = ginkgo.Describe("Artemis Queue Test with AMQP", func() {
+    var client *amqp.Client
+    var session *amqp.Session
+    var sender *amqp.Sender
+    var receiver *amqp.Receiver
+    var ctx context.Context
+    var err error
+
+    ginkgo.BeforeEach(func() {
+        ctx = context.Background()
+        client, err = amqp.Dial("amqp://ex-aao-hdls-svc.activemq-artemis-brokers.svc.cluster.local:61619", amqp.ConnSASLPlain("cgi", "cgi")) // Replace with actual credentials and Artemis server address
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+        session, err = client.NewSession()
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+    })
+
+    ginkgo.It("should send and receive two messages in a queue", func() {
+        queueName := "TESTKUBE"
+        firstMessageText := "Hi, this is message 1"
+        secondMessageText := "Hi, this is message 2"
+    
+        // Create a sender
+        sender, err = session.NewSender(
+            amqp.LinkTargetAddress(queueName),
+        )
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+    
+        // Send the first message (case 1)
+        err = sender.Send(ctx, amqp.NewMessage([]byte(firstMessageText)))
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+    
+        // Wait for 10 minutes after sending the first message (case 2)
+        time.Sleep(1 * time.Minute)
+
+        // Create a receiver for the first message
+        receiver, err = session.NewReceiver(
+            amqp.LinkSourceAddress(queueName),
+        )
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+    
+        // Receive the first message
+        msg, err := receiver.Receive(ctx)
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+        gomega.Expect(string(msg.GetData())).To(gomega.Equal(firstMessageText))
+    
+        // Accept the first message
+        msg.Accept()
+    
+        // Close the first receiver to clean up before receiving the second message
+        receiver.Close(ctx)
+    
+        // Send the second message (case 3)
+        err = sender.Send(ctx, amqp.NewMessage([]byte(secondMessageText)))
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+    
+        // Create a receiver for the second message
+        receiver, err = session.NewReceiver(
+            amqp.LinkSourceAddress(queueName),
+        )
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+    
+        // Receive the second message
+        msg, err = receiver.Receive(ctx)
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
+        gomega.Expect(string(msg.GetData())).To(gomega.Equal(secondMessageText))
+    
+        // Accept the second message
+        msg.Accept()
+    })
+
+    ginkgo.AfterEach(func() {
+        if sender != nil {
+            sender.Close(ctx)
+        }
+        if receiver != nil {
+            receiver.Close(ctx)
+        }
+        if session != nil {
+            session.Close(ctx)
+        }
+        if client != nil {
+            client.Close()
+        }
+    })
+})
---
+++ ArtemisTests/Ginkgo/RoshanTest/checkoperator_test.go
@@ -0,0 +1,40 @@
+package RoshanTest_test
+
+import (
+	"context"
+	"fmt"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"strings"
+)
+
+var _ = Describe("Kafka Broker Pods", func() {
+	It("should have the correct 'activemq-artemis-controller-manager-' prefixed pods running", func() {
+		config, err := rest.InClusterConfig()
+		Expect(err).To(BeNil(), "Error getting in-cluster config: %v", err)
+
+		clientset, err := kubernetes.NewForConfig(config)
+		Expect(err).To(BeNil(), "Error creating Kubernetes client: %v", err)
+
+		namespace := "activemq-artemis-operator"
+		expectedPodPrefix0 := "activemq-artemis-controller-manager-"
+
+		pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+		Expect(err).To(BeNil(), "Error getting pods: %v", err)
+
+		var actualPodCount int
+		for _, pod := range pods.Items {
+			if strings.HasPrefix(pod.Name, expectedPodPrefix0) && pod.Status.Phase == "Running" {
+				fmt.Printf("Pod Name0: %s\n", pod.Name)
+				actualPodCount++
+			}
+		}
+
+		// Set your expected number of 'kafka-brokers-' pods here
+		expectedPodCount := 1 
+		Expect(actualPodCount).To(Equal(expectedPodCount), "Expected %d 'activemq-artemis-controller-manager-' pods, but found %d", expectedPodCount, actualPodCount)
+	})
+})
