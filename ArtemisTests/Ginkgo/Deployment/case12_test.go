1196a5bde761747e5de7ea7944115c3e35ac3d48
Sabrine <sabrine7b@hotmail.com>
2024-01-12 10:53:47 +0100

    Update case12_test.go

--- ArtemisTests/Ginkgo/Deployment/case12_test.go
+++ ArtemisTests/Ginkgo/Deployment/case12_test.go
@@ -1,27 +1,16 @@
 package Deployment_test
 
 import (
-	"context"
 	"fmt"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/util/homedir"
-	"path/filepath"
 	"io/ioutil"
+	"path/filepath"
 	"gopkg.in/yaml.v2"
 )
 
 var _ = Describe("Check ClusterIssuer Existence", func() {
 	It("should ensure ClusterIssuers exist in the YAML files associated with certificates", func() {
-		config, err := rest.InClusterConfig()
-		Expect(err).To(BeNil(), "Error getting in-cluster config: %v", err)
-
-		clientset, err := kubernetes.NewForConfig(config)
-		Expect(err).To(BeNil(), "Error creating Kubernetes client: %v", err)
-
 		// Specify the path to your YAML file containing certificates and ClusterIssuers
 		yamlFilePath := filepath.Join(homedir.HomeDir(), "path/to/your/file.yaml")
 
