ebb940f5fa20aee7c60e478e1a6cf08e8c80c47c
Sabrine <sabrine7b@hotmail.com>
2024-01-12 10:51:16 +0100

    Update case12_test.go

--- ArtemisTests/Ginkgo/Deployment/case12_test.go
+++ ArtemisTests/Ginkgo/Deployment/case12_test.go
@@ -8,58 +8,52 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
 )
 
 var _ = Describe("Check ClusterIssuer Existence", func() {
-	It("should ensure the ClusterIssuer 'amq-ca-issuer' exists in the 'activemq-artemis-brokers' namespace", func() {
-		// Get in-cluster config
+	It("should ensure ClusterIssuers exist in the YAML files associated with certificates", func() {
 		config, err := rest.InClusterConfig()
 		Expect(err).To(BeNil(), "Error getting in-cluster config: %v", err)
 
-		// Create Kubernetes client
 		clientset, err := kubernetes.NewForConfig(config)
 		Expect(err).To(BeNil(), "Error creating Kubernetes client: %v", err)
 
-		// Specify namespace and ClusterIssuer name
-		namespace := "activemq-artemis-brokers"
-		issuerName := "amq-ca-issuer"
+		// Specify the path to your YAML file containing certificates and ClusterIssuers
+		yamlFilePath := filepath.Join(homedir.HomeDir(), "path/to/your/file.yaml")
 
-		// Check if the ClusterIssuer exists
-		_, err = clientset.AdmissionregistrationV1().ClusterIssuers().Get(context.TODO(), issuerName, metav1.GetOptions{})
-		if err != nil {
-			// ClusterIssuer not found
-			fmt.Printf("ClusterIssuer '%s' not found in namespace '%s'\n", issuerName, namespace)
-			Expect(err).ToNot(HaveOccurred(), "Expected ClusterIssuer to be missing, but got an error.")
-		} else {
-			// ClusterIssuer found
-			fmt.Printf("ClusterIssuer '%s' found in namespace '%s'\n", issuerName, namespace)
-			Expect(err).To(BeNil(), "Expected ClusterIssuer to exist, but got an error.")
-		}
-	})
+		// Read the YAML file
+		yamlContent, err := ioutil.ReadFile(yamlFilePath)
+		Expect(err).To(BeNil(), "Error reading YAML file: %v", err)
 
-	It("should ensure the ClusterIssuer 'root-secret' exists in the 'cert-manager' namespace", func() {
-		// Get in-cluster config
-		config, err := rest.InClusterConfig()
-		Expect(err).To(BeNil(), "Error getting in-cluster config: %v", err)
+		// Unmarshal YAML content into a map
+		var yamlData map[string]interface{}
+		err = yaml.Unmarshal(yamlContent, &yamlData)
+		Expect(err).To(BeNil(), "Error unmarshalling YAML content: %v", err)
 
-		// Create Kubernetes client
-		clientset, err := kubernetes.NewForConfig(config)
-		Expect(err).To(BeNil(), "Error creating Kubernetes client: %v", err)
+		// Check if the YAML content contains certificates and ClusterIssuers
+		certificates, foundCertificates := yamlData["Certificates"].(map[string]interface{})
+		Expect(foundCertificates).To(BeTrue(), "No certificates found in the YAML file.")
 
-		// Specify namespace and ClusterIssuer name
-		namespace := "cert-manager"
-		issuerName := "root-secret"
+		for certName, certData := range certificates {
+			// Assuming the YAML structure, adjust it based on your actual structure
+			clusterIssuerName, found := certData.(map[string]interface{})["ClusterIssuer"].(string)
+			Expect(found).To(BeTrue(), "ClusterIssuer not found for certificate %s", certName)
 
-		// Check if the ClusterIssuer exists
-		_, err = clientset.AdmissionregistrationV1().ClusterIssuers().Get(context.TODO(), issuerName, metav1.GetOptions{})
-		if err != nil {
-			// ClusterIssuer not found
-			fmt.Printf("ClusterIssuer '%s' not found in namespace '%s'\n", issuerName, namespace)
-			Expect(err).ToNot(HaveOccurred(), "Expected ClusterIssuer to be missing, but got an error.")
-		} else {
-			// ClusterIssuer found
-			fmt.Printf("ClusterIssuer '%s' found in namespace '%s'\n", issuerName, namespace)
-			Expect(err).To(BeNil(), "Expected ClusterIssuer to exist, but got an error.")
+			// Check if the ClusterIssuer exists in the YAML content
+			_, found = yamlData["ClusterIssuers"].(map[string]interface{})[clusterIssuerName]
+			if !found {
+				// ClusterIssuer not found
+				fmt.Printf("ClusterIssuer '%s' not found in the YAML file\n", clusterIssuerName)
+				Expect(found).To(BeTrue(), "Expected ClusterIssuer to be present, but it is missing.")
+			} else {
+				// ClusterIssuer found
+				fmt.Printf("ClusterIssuer '%s' found in the YAML file\n", clusterIssuerName)
+				Expect(found).To(BeTrue(), "Expected ClusterIssuer to be present, but it is missing.")
+			}
 		}
 	})
 })
